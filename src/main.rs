5287f1524536046f120a093e7816773ec6032505
dnabre <alexander@happybunnylabs.com>
2022-12-08 12:09:42 -0700

    Day 08, part 1 and part 2 completed cleaned up and generalized

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 #![allow(unused_mut)]
 
 
-extern crate core;
 
 
 use std::fs;
@@ -21,7 +20,7 @@ use ndarray::{Array, ArrayBase, OwnedRepr, Dim, Array2, Axis, s};
 /*
     Advent of Code 2022: Day 08
         part1 answer: 1538
-        part2 answer:
+        part2 answer: 496125
  */
 
 
@@ -39,6 +38,11 @@ const PART1_INPUT_FILENAME: &str = "data/day08/part1_input.txt";
 const PART2_TEST_FILENAME: &str = "data/day08/part2_test.txt";
 const PART2_INPUT_FILENAME: &str = "data/day08/part2_input.txt";
 
+enum Direction {
+    Left,Right,Up,Down
+}
+
+
 fn main() {
     print!("Advent of Code 2022, Day ");
     println!("08");                           // insert Day
@@ -48,10 +52,10 @@ fn main() {
     let duration1 = start1.elapsed();
     println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
 
-    // let start2 = Instant::now();
-    // let answer2 = part2();
-    // let duration2 = start2.elapsed();
-    // println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
+    let start2 = Instant::now();
+    let answer2 = part2();
+    let duration2 = start2.elapsed();
+    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
 
     println!("----------\ndone");
 }
@@ -115,6 +119,48 @@ fn vertical_sweep(tree_array: &Array2<i32>, vis: &mut ArrayBase<OwnedRepr<i32>, 
 }
 
 
+fn get_scenic_score(tree_array: &Array2<i32>) -> i32 {
+    let mut scenic_score: i32 = 0;
+    let size = tree_array.dim().0;
+    let (mut up,mut down,mut left,mut right):(i32,i32,i32,i32);
+    for i in 1..size - 1 {
+        for j in 1..size - 1 {
+            let tree_height = tree_array[[i,j]];
+
+            up = scenic_direction(&tree_array,Direction::Up,i,j,tree_height);
+            down = scenic_direction(&tree_array,Direction::Down,i,j,tree_height);
+            left = scenic_direction(&tree_array,Direction::Left,i,j,tree_height);
+            right= scenic_direction(&tree_array,Direction::Right,i,j,tree_height);
+
+            if up * right * down * left > scenic_score {
+                scenic_score = up * right * down * left;
+            }
+        }
+    }
+   return scenic_score;
+}
+
+fn scenic_direction(tree_array:  &Array2<i32>, dir: Direction, i:usize, j:usize, tree_height:i32) -> i32 {
+    let size = tree_array.dim().0;
+    let range:Vec<usize> = match dir {
+        Direction::Left => (0..j).rev().collect(),
+        Direction::Right => (j + 1..size).collect(),
+        Direction::Up => (0..i).rev().collect(),
+        Direction::Down => (i + 1..size).collect()
+    };
+    let mut s_value = 0;
+
+    for n in range {
+        s_value += 1;
+        if tree_array[[n, j]] < tree_height {
+            continue;
+        } else {
+            break;
+        }
+    }
+    return s_value;
+}
+
 
 fn part1() -> String {
     let p1_file = match TEST {
@@ -147,12 +193,10 @@ fn part1() -> String {
     let mut tree_array:Array2<i32> = Array2::from_shape_vec ((size, size), n_array).unwrap();
     let (rows,cols):(usize,usize) = tree_array.dim();
 
-    //  println!("trees {rows}x{cols} : \n{}", tree_array);
     let mut vis: ArrayBase<OwnedRepr<i32>, Dim<[usize; 2]>> = Array::zeros((size, size));
 
 
     horizontal_sweep( &tree_array, &mut vis);
-
     vertical_sweep(&mut tree_array, &mut vis);
 
     let mut vis_count = 0;
@@ -176,12 +220,32 @@ fn part2() -> String {
         fs::read_to_string(p2_file).expect(&*format!("error opening file {}", p2_file));
 
 
-    let lines: Vec<&str> = data2_s.trim().split("\n").collect();
+    let mut lines: Vec<&str> = data2_s.trim().split("\n").collect();
+    lines = lines.iter().map(|l| l.trim()).collect();
     let l_num = lines.len();
+
     if TEST {
         println!("\t read {} lines from {}", l_num, p2_file);
     }
 
-    let mut answer2 = String::new();
+
+    let size = lines[0].len();
+    let mut sbuffer = String::new();
+
+    for ln in lines {
+        sbuffer.push_str(ln);
+    }
+    let mut n_array:Vec<i32> = Vec::new();
+    let b_chars  = sbuffer.chars();
+    for ch in b_chars {
+        if let Some(digit) = ch.to_digit(10) {
+            n_array.push(digit as i32);
+        }
+    }
+
+    let mut tree_array:Array2<i32> = Array2::from_shape_vec ((size, size), n_array).unwrap();
+    let scenic = get_scenic_score(&tree_array);
+    let mut answer2 =scenic.to_string();
+
     return answer2;
 }
