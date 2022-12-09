929d04b657a2852e7603d5b7bdcbbc45d2c8c915
dnabre <alexander@happybunnylabs.com>
2022-12-09 09:38:25 -0700

    working on Day07 (currently src/main.rs)
    Correct result for Part 1
    tons of debugging output that needs cleaned up.

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,25 @@
 
 use std::fs;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::time::Instant;
 use std::iter;
 use std::ops::{Add, Index};
 use std::fmt;
-use std::path::PathBuf;
+
 use std::thread::current;
 use parse_display::FromStr;
 
 /*
     Advent of Code 2022: Day 07
-        part1 answer:
+        part1 answer: 1581595
+            1835142 is too high
         part2 answer:
+
+
+        part1, other guy:
+            194
+            1581595
  */
 
 
@@ -32,18 +39,23 @@ const P_DELIMIT: &'static str = "\\";
 const P_DELIMIT: &'static str = "/";
 
 
-const TEST: bool = true;
+const TEST: bool = false;
 
 const PART1_TEST_FILENAME: &str = "data/day07/part1_test.txt";
+const PART1_TEST2_FILENAME: &str = "data/day07/part1_test_2.txt";
 const PART1_INPUT_FILENAME: &str = "data/day07/part1_input.txt";
 
 const PART2_TEST_FILENAME: &str = "data/day07/part2_test.txt";
 const PART2_INPUT_FILENAME: &str = "data/day07/part2_input.txt";
 
+
+const PART1_DIR_SIZE_CAP: u32 = 100_000;
 fn main() {
     print!("Advent of Code 2022, Day ");
     println!("07");                           // insert Day
 
+
+   // test();
     let start1 = Instant::now();
     let answer1 = part1();
     let duration1 = start1.elapsed();
@@ -62,7 +74,7 @@ fn main() {
 #[display("{size} {name}")]
 struct File {
     name: String,
-    size: usize,
+    size: u32,
 }
 
 
@@ -71,6 +83,23 @@ struct File {
 struct Dir {
     name: String,
 }
+
+fn test() {
+    let strings = ["/bcfwbq/bsbpc/", "/", "gpbswq","/bcfwbq/bsbpc","gpbswq/" ];
+    for s in strings {
+        let mut ss = String::from(s);
+
+        println!("plain {ss}");
+        let ch:char = ss.chars().last().unwrap();
+        println!("last char: |{}|", ch);
+        println!("result of last_char_is(&ss, '/') ->  {}", last_char_is(&ss, '/'));
+
+        println!("--------------\n\n");
+    }
+
+
+}
+
 
 
 fn part1() -> String {
@@ -90,63 +119,203 @@ fn part1() -> String {
         lines[i] = lines[i].trim();
     }
 
-    let mut file_size: HashMap<PathBuf, u32> = HashMap::new();
-    let mut dir_size: HashMap<PathBuf, u32> = HashMap::new();
+    let mut dir_size: HashMap<String, u32> = HashMap::new();
+
+  //  let mut directory_list:Vec<String> = Vec::new();
+    let mut directory_set:HashSet<String> = HashSet::new();
 
 
-    let mut path: PathBuf;
-    if P_DELIMIT.eq("\\") {
-        path = PathBuf::from(r"\");
-    } else {
-        path = PathBuf::from(r"/");
-    }
-    let root = path.clone();
-    println!("root: {:?}", root);
 
+    let mut cwd = String::from("/");
+    directory_set.insert(cwd.clone());
+    dir_size.insert(cwd.clone(), 0);
+
+    let mut line_count = 0;
     // Assume first command will be "cd /"
     for line in lines {
+        line_count += 1;
+  //      if line_count > 40  {break};
+        println!("cwd: {cwd} \t line: {line} \t\t l_num: {line_count}");
         if line.starts_with("$") {
             //command
             if line.starts_with("$ ls") {
-                println!("listing  : {:?}", path);
+                println!("listing  : {cwd}");
             } else {  // change directory command
+                if !cwd.ends_with("/") {
+                    println!("{cwd} doesn't end with /  , while processing {line_count}");
+             //       panic!();
+                }
+                let mut debug = false;
                 let (_, dir) = line.split_once("cd ").unwrap();
-                print!("cd from {:?} to ", path);
+                println!("cd from {cwd}, {dir}");
+                if dir.ends_with("gpbswq") {
+                    println!("error here");
+                    debug = true;
+                }
                 match dir {
                     "/" => {
-                        path = root.clone();
-                        println!("{:?}", path);
+                        cwd = String::from("/");
+
                     }
                     ".." => {
-                         path.pop();
-                        println!("{:?}", path);
+                        if cwd.ne("/") {
+                           // let p_cwd = dir_up(&mut cwd);
+                           // cwd = p_cwd;
+                          //
+                            cwd = dir_up(&cwd);
+                          }
                     }
                     d_name => {
-
-                        path.push(d_name);
-                        println!("{:?}", path);
+                        if debug {println!("\t cd __ branch, cwd: {cwd}, d_name: {d_name}")};
+                    //    if cwd.ne("/") { cwd.push('/'); }
+                        cwd.push_str(d_name);
+                        if debug {println!("push_str d_name ({d_name}) onto cwd, resulting in {cwd}")};
+                        cwd.push('/');
+                        if debug {println!("pushed slash onto cwd, resulting {cwd}")};
+                        let rr = directory_set.insert(cwd.clone());
+                        if debug {println!("inserted clone of cwd into directory_set, result {}", rr)};
+                        let rr2 = dir_size.insert(cwd.clone(), 0);
+                        if debug {println!("insert cwd = 0 , {cwd} into dir_size, result {:?}", rr2)};
                     }
-                }
-            }
-            //   println!("\t\t post: {:?}", path);
 
+                }
+                println!("moved to {cwd}");
+            }
             //end command
         } else {
             //listing
+
+
+
             if line.starts_with("dir") {
                 let d: Dir = line.parse().unwrap();
-                println!("list  dir: {} \t -:- {:?} ", d.name, path);
+                println!("list  dir: {} \t -:- {cwd} ", d.name);
 
             } else {
+                let mut c_size =  match dir_size.get(&cwd) {
+                    None => {0}
+                    Some(&x) => {x}
+                };
                 let f: File = line.parse().unwrap();
-                println!("list file: {} \t -:- {:?} size: {}", f.name, path, f.size);
+                println!("\t File: {cwd} <|> {} \t{}", f.name,f.size);
+                c_size = c_size +  f.size;
+                let r = dir_size.insert(cwd.clone(),c_size);
+
             }
         }
     }
 
+    println!("\n directory list:");
+    for d in &directory_set {
+        let size = dir_size.get(d);
+        match size {
+            None => {
+                println!("\t{d:<20}  \t size: (unknown)");
+                panic!("directory {d} should have known size");
+            }
+            Some(s) => {
+   //             println!("\t{d:<20}  \t size: {s}");
+            }
+        }
 
-    let mut answer1 = String::new();
+    }
+
+
+    let cum_dir_sizes:HashMap<String,u32> =
+        get_cumulative_directory_sizes(&directory_set, &dir_size);
+
+    let mut total_size:u32 = 0;
+
+    println!("\n directory list:");
+    for d in &directory_set {
+        let size = cum_dir_sizes.get(d);
+        match size {
+            None => {
+                println!("\t{d:<20}  \t size: (unknown)");
+                panic!("directory {d} should have known size");
+            }
+            Some(&s) => {
+                if s < PART1_DIR_SIZE_CAP{
+                    total_size += s;
+
+                }
+       //        println!("\t{d:<20}  \t size: {s}");
+            }
+        }
+
+    }
+
+    let mut with_slash=0;
+    let mut without_slash = 0;
+    for d in &directory_set {
+        if last_char_is(d, '/') {
+            with_slash +=1 ;
+        } else {
+            without_slash +=1;
+        }
+    }
+    println!("directories ending with slash: {with_slash}, those without: {without_slash}");
+
+    println!("directory entries: {}", directory_set.len());
+    let mut answer1 = total_size.to_string();
     return answer1;
+}
+
+
+fn get_cumulative_directory_sizes(dirs_set: &HashSet<String>, dir_size: &HashMap<String, u32>) -> HashMap<String, u32> {
+    let mut cum_size_map:HashMap<String,u32> = HashMap::new();
+
+    for dir in dirs_set {
+        let cwd = dir.clone();
+        let mut size:u32 = 0;
+        // println!("scanning for subdirs of {cwd}");
+        for sub_dir in dirs_set {
+            let sd_size = match dir_size.get(sub_dir) {
+                None => {
+                    panic!("can't find size for {sub_dir}");
+                }
+                Some(x) => {x}
+            };
+            // println!("\t subdir: {sub_dir:<20} \t size: {sd_size} ");
+            if sub_dir.starts_with(&cwd) {
+                let new_size = sd_size + size;
+                // println!("\t\t {sub_dir} is inside {dir}, adding it's size: {} + {} => {}", sd_size, size, new_size);
+                size = new_size;
+            }
+        }
+        cum_size_map.insert(cwd, size);
+
+    }
+
+    return cum_size_map;
+}
+
+
+fn last_char_is(s:&String, ch: char)->bool {
+    let c = s.chars().last().unwrap();
+    if c==ch {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn dir_up(cwd: & String) -> String {
+    let mut p_cwd = cwd.clone();
+    p_cwd.pop();
+    let (left, _) = p_cwd.rsplit_once("/").unwrap();
+    if left.is_empty() {
+        p_cwd = String::from("/");
+    } else {
+        p_cwd = String::from(left);
+    }
+    if !last_char_is(&p_cwd, '/') {
+        println!("-----------------------------> doing 'cd ..' and result {p_cwd} doesn't end in /");
+        p_cwd.push('/');
+    } else {
+        println!("--------------> doing 'cd ..' and result {p_cwd}, end with slash properly");
+    }
+    return p_cwd
 }
 
 
