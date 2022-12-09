53b3c09743d9fd0e2aa3235a684bf3afd93422aa
dnabre <alexander@happybunnylabs.com>
2022-12-09 11:52:42 -0700

    Day 07, part 1 and part 2 completed clean up needed.

--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,21 @@ use std::time::Instant;
 use std::iter;
 use std::ops::{Add, Index};
 use std::fmt;
-
 use std::thread::current;
+use thousands::Separable;
 use parse_display::FromStr;
 
 /*
     Advent of Code 2022: Day 07
         part1 answer: 1581595
-            1835142 is too high
-        part2 answer:
-
-
-        part1, other guy:
-            194
-            1581595
-            Total sum of directories < 100000: 1581595
-Directory to free: ////bcfwbq/gpbswq/tnsjg/jrflhz 1544176
+        part2 answer: 1544176
 
  */
 
-
-#[cfg(windows)]
-const D_LINE_ENDING: &'static str = "\r\n\r\n";
-#[cfg(not(windows))]
-const D_LINE_ENDING: &'static str = "\n\n";
-
-#[cfg(windows)]
-const P_DELIMIT: &'static str = "\\";
-#[cfg(not(windows))]
-const P_DELIMIT: &'static str = "/";
+const TEST_ANSWER:(u32,u32) =(95437,24933642);
+const INPUT_ANSWER:(u32,u32) = (1581595,1544176);
 
 
-const TEST: bool = true;
 
 const PART1_TEST_FILENAME: &str = "data/day07/part1_test.txt";
 const PART1_TEST2_FILENAME: &str = "data/day07/part1_test_2.txt";
@@ -57,6 +40,9 @@ const PART2_TOTAL_SPACE: u32 = 70_000_000;
 const PART2_TARGET_UNUSED_SPACE: u32 = 30_000_000;
 
 
+
+const TEST: bool = false;
+
 fn main() {
     print!("Advent of Code 2022, Day ");
     println!("07");                           // insert Day
@@ -67,11 +53,22 @@ fn main() {
     let answer1 = part1();
     let duration1 = start1.elapsed();
     println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
+    if TEST {
+        assert_eq!(answer1, TEST_ANSWER.0.to_string());
+    } else {
+        assert_eq!(answer1, INPUT_ANSWER.0.to_string());
+    }
 
-    // let start2 = Instant::now();
-    // let answer2 = part2();
-    // let duration2 = start2.elapsed();
-    // println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
+    let start2 = Instant::now();
+    let answer2 = part2();
+    let duration2 = start2.elapsed();
+    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
+    if TEST {
+        assert_eq!(answer2, TEST_ANSWER.1.to_string());
+    } else {
+        assert_eq!(answer2, INPUT_ANSWER.1.to_string());
+    }
+
 
     println!("----------\ndone");
 }
@@ -110,96 +107,9 @@ fn part1() -> String {
         lines[i] = lines[i].trim();
     }
 
-    let mut dir_size: HashMap<String, u32> = HashMap::new();
 
-    let mut directory_set:HashSet<String> = HashSet::new();
-
-
-
-    let mut cwd = String::from("/");
-    directory_set.insert(cwd.clone());
-    dir_size.insert(cwd.clone(), 0);
-
-    let mut line_count = 0;
-    // Assume first command will be "cd /"
-    for line in lines {
-        line_count += 1;
- //       println!("cwd: {cwd} \t line: {line} \t\t l_num: {line_count}");
-        if line.starts_with("$") {
-            //command
-            if line.starts_with("$ ls") {
-   //             println!("listing  : {cwd}");
-            } else {  // change directory command
-                let mut debug = false;
-                let (_, dir) = line.split_once("cd ").unwrap();
-            //    println!("cd from {cwd}, {dir}");
-                match dir {
-                    "/" => {
-                        cwd = String::from("/");
-
-                    }
-                    ".." => {
-                        if cwd.ne("/") {
-                           // let p_cwd = dir_up(&mut cwd);
-                           // cwd = p_cwd;
-                          //
-                            cwd = dir_up(&cwd);
-                          }
-                    }
-                    d_name => {
-                        if debug {println!("\t cd __ branch, cwd: {cwd}, d_name: {d_name}")};
-                    //    if cwd.ne("/") { cwd.push('/'); }
-                        cwd.push_str(d_name);
-                        if debug {println!("push_str d_name ({d_name}) onto cwd, resulting in {cwd}")};
-                        cwd.push('/');
-                        if debug {println!("pushed slash onto cwd, resulting {cwd}")};
-                        let rr = directory_set.insert(cwd.clone());
-                        if debug {println!("inserted clone of cwd into directory_set, result {}", rr)};
-                        let rr2 = dir_size.insert(cwd.clone(), 0);
-                        if debug {println!("insert cwd = 0 , {cwd} into dir_size, result {:?}", rr2)};
-                    }
-
-                }
-      //          println!("moved to {cwd}");
-            }
-            //end command
-        } else {
-            //listing
-
-
-
-            if line.starts_with("dir") {
-                let d: Dir = line.parse().unwrap();
-        //        println!("list  dir: {} \t -:- {cwd} ", d.name);
-
-            } else {
-                let mut c_size =  match dir_size.get(&cwd) {
-                    None => {0}
-                    Some(&x) => {x}
-                };
-                let f: File = line.parse().unwrap();
-          //      println!("\t File: {cwd} <|> {} \t{}", f.name,f.size);
-                c_size = c_size +  f.size;
-                let r = dir_size.insert(cwd.clone(),c_size);
-
-            }
-        }
-    }
-
-  //  println!("\n directory list:");
-    for d in &directory_set {
-        let size = dir_size.get(d);
-        match size {
-            None => {
-                println!("\t{d:<20}  \t size: (unknown)");
-                panic!("directory {d} should have known size");
-            }
-            Some(s) => {
-   //             println!("\t{d:<20}  \t size: {s}");
-            }
-        }
-
-    }
+    let (mut dir_size, mut directory_set):(HashMap<String,u32>,HashSet<String> )
+        = parse_into_directories(&mut lines);
 
 
     let cum_dir_sizes:HashMap<String,u32> =
@@ -230,6 +140,82 @@ fn part1() -> String {
     println!("directory entries: {}", directory_set.len());
     let mut answer1 = total_size.to_string();
     return answer1;
+}
+
+
+// ctor_set):(HashMap<String,u32>,HashSet<String> )
+
+fn parse_into_directories(lines: &mut Vec<&str>) -> (HashMap<String,u32>, HashSet<String>){
+    let mut dir_size: HashMap<String, u32> = HashMap::new();
+    let mut directory_set: HashSet<String> = HashSet::new();
+
+    let mut cwd = String::from("/");
+    directory_set.insert(cwd.clone());
+    dir_size.insert(cwd.clone(), 0);
+
+    let mut line_count = 0;
+    // Assume first command will be "cd /"
+    for line in lines {
+        line_count += 1;
+        //       println!("cwd: {cwd} \t line: {line} \t\t l_num: {line_count}");
+        if line.starts_with("$") {
+            //command
+            if line.starts_with("$ ls") {
+                //             println!("listing  : {cwd}");
+            } else {  // change directory command
+                let mut debug = false;
+                let (_, dir) = line.split_once("cd ").unwrap();
+                //    println!("cd from {cwd}, {dir}");
+                match dir {
+                    "/" => {
+                        cwd = String::from("/");
+                    }
+                    ".." => {
+                        if cwd.ne("/") {
+                            // let p_cwd = dir_up(&mut cwd);
+                            // cwd = p_cwd;
+                            //
+                            cwd = dir_up(&cwd);
+                        }
+                    }
+                    d_name => {
+                        if debug { println!("\t cd __ branch, cwd: {cwd}, d_name: {d_name}") };
+                        //    if cwd.ne("/") { cwd.push('/'); }
+                        cwd.push_str(d_name);
+                        if debug { println!("push_str d_name ({d_name}) onto cwd, resulting in {cwd}") };
+                        cwd.push('/');
+                        if debug { println!("pushed slash onto cwd, resulting {cwd}") };
+                        let rr = directory_set.insert(cwd.clone());
+                        if debug { println!("inserted clone of cwd into directory_set, result {}", rr) };
+                        let rr2 = dir_size.insert(cwd.clone(), 0);
+                        if debug { println!("insert cwd = 0 , {cwd} into dir_size, result {:?}", rr2) };
+                    }
+                }
+                //          println!("moved to {cwd}");
+            }
+            //end command
+        } else {
+            //listing
+
+
+
+            if line.starts_with("dir") {
+                let d: Dir = line.parse().unwrap();
+                //        println!("list  dir: {} \t -:- {cwd} ", d.name);
+            } else {
+                let mut c_size = match dir_size.get(&cwd) {
+                    None => { 0 }
+                    Some(&x) => { x }
+                };
+                let f: File = line.parse().unwrap();
+                //      println!("\t File: {cwd} <|> {} \t{}", f.name,f.size);
+                c_size = c_size + f.size;
+                let r = dir_size.insert(cwd.clone(), c_size);
+            }
+        }
+    }
+
+   return (dir_size,directory_set);
 }
 
 
@@ -271,7 +257,8 @@ fn last_char_is(s:&String, ch: char)->bool {
     }
 }
 
-fn dir_up(cwd: & String) -> String {
+/*
+ fn dir_up(cwd: & String) -> String {
     let mut p_cwd = cwd.clone();
     p_cwd.pop();
     let (left, _) = p_cwd.rsplit_once("/").unwrap();
@@ -282,7 +269,19 @@ fn dir_up(cwd: & String) -> String {
     }
     return p_cwd
 }
-
+*/
+fn dir_up(cwd: & String) -> String {
+    let mut p_cwd = cwd.clone();
+    p_cwd.pop();
+    let (left, _) = p_cwd.rsplit_once("/").unwrap();
+    if left.is_empty() {
+        p_cwd = String::from("/");
+    } else {
+        p_cwd = String::from(left);
+    }
+    p_cwd.push('/');
+      return p_cwd;
+}
 
 fn part2() -> String {
     let p2_file = match TEST {
@@ -293,12 +292,48 @@ fn part2() -> String {
         fs::read_to_string(p2_file).expect(&*format!("error opening file {}", p2_file));
 
 
-    let lines: Vec<&str> = data2_s.trim().split("\n").collect();
+    let mut lines: Vec<&str> = data2_s.trim().split("\n").collect();
     let l_num = lines.len();
     if TEST {
         println!("\t read {} lines from {}", l_num, p2_file);
     }
 
-    let mut answer2 = String::new();
+    for i in 0..lines.len() {
+        lines[i] = lines[i].trim();
+    }
+
+
+    let (mut dir_size, mut directory_set):(HashMap<String,u32>,HashSet<String> )
+        = parse_into_directories(&mut lines);
+
+
+    let cum_dir_sizes:HashMap<String,u32> =
+        get_cumulative_directory_sizes(&directory_set, &dir_size);
+    let mut root_used = cum_dir_sizes.get("/").unwrap();
+    let mut free_space = PART2_TOTAL_SPACE - root_used;
+   let mut need_to_free =   PART2_TARGET_UNUSED_SPACE - free_space;
+
+
+
+    let mut dir_size_totals:Vec<&u32> = cum_dir_sizes.values().collect();
+ //   println!("{:?}", dir_size_totals);
+
+
+    let mut size_values:Vec<u32> = dir_size_totals.iter().map(|x| **x).collect();
+    size_values.sort();
+
+    let mut to_free = 0;
+
+    let mut to_free = 0;
+    for i in 0..size_values.len() {
+        let v = size_values[i];
+        if v >= need_to_free {
+            to_free = v;
+            break;
+        }
+    }
+    // target answer 1544176
+
+    let mut answer2 = to_free.to_string();
     return answer2;
 }
