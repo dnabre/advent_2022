6faf941475a464cb6760ab38a366f2959f2a6219
matthew j. barrick <alexander@happybunnylabs.com>
2022-12-10 21:18:03 -0700

    Day10
    fixed off-by-one error that was making part2 output ugly but readable.
    inital debug cleanup

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,21 @@ use parse_display::FromStr;
  */
 
 
-const TEST_ANSWER: (u32, u32) = (13140, 0);
-const INPUT_ANSWER: (u32, u32) = (15220, 0);
+const TEST_ANSWER: (u32,&str) = (13140, concat!(
+        "██..██..██..██..██..██..██..██..██..██..\n",
+        "███...███...███...███...███...███...███.\n",
+        "████....████....████....████....████....\n",
+        "█████.....█████.....█████.....█████.....\n",
+        "██████......██████......██████......████\n",
+        "███████.......███████.......███████.....\n"));
+
+const INPUT_ANSWER: (u32, &str) = (15220, concat!(
+        "███..████.████.████.█..█.███..████..██..\n",
+        "█..█.█.......█.█....█.█..█..█.█....█..█.\n",
+        "█..█.███....█..███..██...███..███..█..█.\n",
+        "███..█.....█...█....█.█..█..█.█....████.\n",
+        "█.█..█....█....█....█.█..█..█.█....█..█.\n",
+        "█..█.█....████.████.█..█.███..█....█..█.\n"));
 
 const PART1_TEST_0_FILENAME: &str = "data/day10/part1_test_0.txt";
 const PART1_TEST_FILENAME: &str = "data/day10/part1_test.txt";
@@ -42,7 +55,7 @@ fn main() {
     let answer1 = part1();
     let duration1 = start1.elapsed();
 
-    println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
+    println!("\t Part 1: {answer1:>20}, time: {:?}", duration1);
 
     if TEST {
         assert_eq!(answer1, TEST_ANSWER.0.to_string());
@@ -53,12 +66,15 @@ fn main() {
     let start2 = Instant::now();
     let answer2 = part2();
     let duration2 = start2.elapsed();
-    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
-    // // if TEST {
-    //     assert_eq!(answer2, TEST_ANSWER.1.to_string());
-    // } else {
-    //     assert_eq!(answer2, INPUT_ANSWER.1.to_string());
-    // }
+    println!("\t Part 2:  {:>20}, time: {:?}", "<>",duration2);
+    println!("{}",answer2);
+
+
+    if TEST {
+        assert_eq!(answer2, TEST_ANSWER.1.to_string());
+    } else {
+        assert_eq!(answer2, INPUT_ANSWER.1.to_string());
+    }
 
 
     println!("----------\ndone");
@@ -71,7 +87,6 @@ struct Machine {
 }
 
 impl fmt::Display for Machine {
-    // This trait requires `fmt` with this exact signature.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "\t machine, cycle {:>12}, register {:>12}", self.cycle_counter, self.register)
     }
@@ -79,7 +94,7 @@ impl fmt::Display for Machine {
 
 impl Default for Machine {
     fn default() -> Machine {
-        Machine { cycle_counter: 1, register: 1 }
+        Machine { cycle_counter: 0, register: 1 }
     }
 }
 
@@ -146,6 +161,8 @@ fn part1() -> String {
     }
     let mut code: Vec<Instruction> = Vec::new();
 
+    code.push(Instruction::WaitState); //fix timing mismatched between part 1 & 2;
+
     for ln in lines {
         let i: Instruction = ln.parse().unwrap();
         match i {
@@ -159,27 +176,26 @@ fn part1() -> String {
     }
 
     let mut m: Machine = Default::default();
-    //   println!("inital machine: {m}\n");
-    let mut r_max = std::i32::MIN;
-    let mut r_min = std::i32::MAX;
+
+
     let mut total_ss = 0;
+
     for i in code {
+
         match i {
             Instruction::Noop => {}
-            Instruction::Addx(x) => { m.register = m.register  + x; }
+            Instruction::Addx(x) => { m.register = m.register + x; }
             Instruction::WaitState => {}
         }
-        r_max = std::cmp::max(r_max,m.register);
-        r_min = std::cmp::min(r_min,m.register);
         m.cycle_counter += 1;
         if m.cycle_counter % 40 == 20 {
             let ss = m.cycle_counter * (m.register as usize);
             total_ss += ss;
+
         }
     }
-    println!("register range: {r_min} - {r_max}");
-    //   println!("\nfinal machine: {m}");
-    //   println!("total signal strength: {total_ss}");
+
+
     let mut answer1 = total_ss.to_string();
     return answer1;
 }
@@ -218,24 +234,22 @@ fn part2() -> String {
     let mut screen: Crt = Crt::new();
     let mut m: Machine = Default::default();
 
-    println!("inital machine: {m}\n");
-
     let mut total_ss = 0;
-    let mut crt_position: usize=0;
-    let mut r_max = std::i32::MIN;
-    let mut r_min = std::i32::MAX;
-
+    let mut crt_position: usize = 0;
 
     let mut sprite_vec = get_sprite_vec(m.register);
-    print!("Sprite position: ");
-    print_sprite_vec(&sprite_vec);
-
     let mut crt = String::new();
 
 
     for i in code {
-
-        let new_crt_char = sprite_vec[crt_position % SCREEN_WIDTH];
+      let inner_sprite_index;
+        inner_sprite_index = ((m.cycle_counter as i32) % 40) - m.register;
+        let new_crt_char;
+        if [-1, 0, 1].contains(&inner_sprite_index) {
+            new_crt_char = '█';
+        } else {
+            new_crt_char = '.';
+        }
         crt.push(new_crt_char);
 
         match i {
@@ -243,41 +257,15 @@ fn part2() -> String {
             Instruction::Addx(x) => { m.register = m.register + x; }
             Instruction::WaitState => {}
         }
-        r_max = std::cmp::max(r_max,m.register);
-        r_min = std::cmp::min(r_min,m.register);
-         // if m.register < 0  {
-         //     println!("{}",crt);
-         //     panic!("{m}");
-         //
-         // }
 
-
-        println!("{m}\n {i}");
-        match i {
-            Instruction::Addx(x) => {
-                if(m.register >= 0) {
-                    sprite_vec = get_sprite_vec(m.register);
-                }
-            }
-            _ => {
-            }
-        }
-
-
-         m.cycle_counter += 1;
-        crt_position +=1 ;
+        m.cycle_counter += 1;
+        crt_position += 1;
         if crt_position % 40 == 0 {
             crt.push('\n');
         }
-
     }
 
-    println!("{}", crt);
-
-    println!("\nfinal machine: {m}");
-    println!("total signal strength: {total_ss}");
-    println!("register range: {r_min} - {r_max}");
-    let mut answer2 = 0.to_string();
+    let mut answer2 = crt;
     return answer2;
 }
 
@@ -292,10 +280,7 @@ fn get_sprite_vec(p: i32) -> Vec<char> {
     let pi = p as usize;
     let mut r: Vec<char> = Vec::new();
     for i in 1..SCREEN_WIDTH + 1 {
-        if i + 1 == (pi + 1) { r.push('#'); }
-        else if i == (pi + 1) { r.push('#'); }
-        else if i - 1 == (pi + 1) { r.push('#'); }
-        else {
+        if i + 1 == (pi + 1) { r.push('#'); } else if i == (pi + 1) { r.push('#'); } else if i - 1 == (pi + 1) { r.push('#'); } else {
             r.push('.');
         }
     }
