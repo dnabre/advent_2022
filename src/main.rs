c6f1d60d9497576f1359a460f573512c4321ee21
matthew j. barrick <alexander@happybunnylabs.com>
2022-12-11 18:02:55 -0700

    Day11, part 1 and part 2 completed.
    Comments/output cleaned up. still need to clear warnings, but saving as template.

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,6 @@ fn main() {
     print!("Advent of Code 2022, Day ");
     println!("11");
 
-
     let start1 = Instant::now();
     let answer1 = part1();
     let duration1 = start1.elapsed();
@@ -54,11 +53,11 @@ fn main() {
     let duration2 = start2.elapsed();
 
     println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
-    // if TEST {
-    //     assert_eq!(answer2, TEST_ANSWER.1.to_string());
-    // } else {
-    //     assert_eq!(answer2, INPUT_ANSWER.1.to_string());
-    //}
+    if TEST {
+        assert_eq!(answer2, TEST_ANSWER.1.to_string());
+    } else {
+        assert_eq!(answer2, INPUT_ANSWER.1.to_string());
+    }
     println!("----------\ndone");
 }
 
@@ -79,7 +78,6 @@ impl fmt::Display for Operation {
             Operation::Square => { '□' }
             Operation::None => { '?' }
         };
-
         write!(f, "{}", op_char)
     }
 }
@@ -99,7 +97,6 @@ struct Monkey {
     false_monkey: usize,
     items_inspected: i64,
 }
-
 
 impl fmt::Display for Monkey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -141,7 +138,6 @@ fn parse_monkeys(mut lines: Vec<&str>) -> Vec<Monkey> {
     let mut operation_line;
     let mut index = 0;
 
-
     while index < lines.len() {
         monkey_parse_buffer.push_str(lines[index]);  // Monkey #:
         monkey_parse_buffer.push('|');
@@ -153,11 +149,8 @@ fn parse_monkeys(mut lines: Vec<&str>) -> Vec<Monkey> {
 
         items_line = lines[index + 1];           // Starting items...
         operation_line = lines[index + 2];       // Operation...
+        index += 7;
 
-
-        index += 6;
-
-        index += 1;
         let mut m: Monkey = monkey_parse_buffer.parse().unwrap();
         m.items = items_line.split_once(":").unwrap().1
             .split(",").map(|p| p.trim().parse().unwrap()).collect();
@@ -173,7 +166,6 @@ fn parse_monkeys(mut lines: Vec<&str>) -> Vec<Monkey> {
                 panic!("Monkey's operation should either Mult or Add at this point.")
             }
         };
-
         let mut op_part = op_pair.1;
         op_part = op_part.trim();
         if op_part.eq("old") {
@@ -181,9 +173,7 @@ fn parse_monkeys(mut lines: Vec<&str>) -> Vec<Monkey> {
         } else {
             m.oper_amount = op_part.parse().unwrap();
         }
-
         monkey.push(m);
-        //  println!("pushed monkey {} after parsing through input {} of {}", mm.monkey_id, index, lines.len());
         monkey_parse_buffer = String::new();
     }
     monkey
@@ -204,33 +194,18 @@ fn part1() -> String {
         println!("\t read {} lines from {}", l_num, p1_file);
     }
 
-
     let mut monkey = parse_monkeys(lines);
-
-   //  let monkey_number = monkey.len();
-   //  let item_number:usize = monkey.iter().map(|m| m.items.len()).sum();
-   //
-   // println!("number of monkeys: {}", monkey_number);
-   //  println!("number of items: {}", item_number);
-   //  for i in 0..monkey.len() {
-   //      println!("monkey[{i:>3}]: {}", monkey[i]);
-   //  }
-
     let number_of_monkeys = monkey.len();
     for round in 1..=20 {
         for i in 0..monkey.len() {
             while !monkey[i].items.is_empty() {
-                // println!("Monkey {}:", monkey[i].monkey_id);
-
                 let init_worry: i64 = match monkey[i].items.pop_front() {
                     None => {
                         panic!("attempted to inspect on monkey without items: {}", monkey[i]);
                     }
                     Some(x) => { x }
                 };
-                // println!(" Monkey inspects an item with worry level of {init_worry}.");
                 monkey[i].items_inspected +=1;
-
                 let mut new_worry = match monkey[i].oper_type {
                     Operation::Mult => { init_worry * monkey[i].oper_amount }
                     Operation::Add => { init_worry + monkey[i].oper_amount }
@@ -239,25 +214,12 @@ fn part1() -> String {
                         panic!("monkey shouldn't have Operation::None, {}", monkey[i]);
                     }
                 };
-
-                // println!("    Worry level is {} to {}.",
-                //          match monkey[i].oper_type {
-                //              Operation::Mult => { format!("multiplied by {}", monkey[i].oper_amount) }
-                //              Operation::Add => { format!("increased by {}", monkey[i].oper_amount) }
-                //              Operation::Square => { format!("multiplied by itself") }
-                //              Operation::None => { panic!("shouldn't have Operation::None, {}", monkey[i]) }
-                //          }, new_worry);
-
                 new_worry = new_worry / 3;
-                // println!("    Monkey gets bored with item. Worry level is divided by 3 to {new_worry}.");
                 if new_worry % monkey[i].test_divider != 0 {
-                    // println!("    Current worry level is not divisible by {} (false)", monkey[i].test_divider);
-                    // println!("    Item with worry level {new_worry} is thrown to monkey {}.",monkey[i].true_monkey);
                     let t_index = monkey[i].false_monkey;
                     monkey[t_index].items.push_back(new_worry);
                 } else {
                     let t_index = monkey[i].true_monkey;
-                    // println!("    Current worry level is divisible by {} (true).", monkey[i].test_divider);
                     monkey[t_index].items.push_back(new_worry);
                 }
             }
@@ -265,13 +227,11 @@ fn part1() -> String {
     }
     let mut i_vec = Vec::new();
     for i in 0..monkey.len() {
-    //    println!("{}", monkey[i]);
         i_vec.push(monkey[i].items_inspected);
     }
     i_vec.sort();
     i_vec.reverse();
     let answer1_i = i_vec[0] * i_vec[1];
-   // println!("{:?}",i_vec);
     let mut answer1 = answer1_i.to_string();
     return answer1;
 }
@@ -292,29 +252,20 @@ fn part2() -> String {
     }
     let mut monkey = parse_monkeys(lines);
     let check_rounds = vec![1,20,1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000];
-    // println!("number of monkeys: {}", monkey.len());
-    // for i in 0..monkey.len() {
-    //     println!("monkey[{i:>3}]: {}", monkey[i]);
-    // }
     let mod_base:i64= monkey.iter().map(|m| m.test_divider).product();
-    println!("using mod_base: {mod_base}");
 
 
     let number_of_monkeys = monkey.len();
     for round in 1..=10000 {
         for i in 0..monkey.len() {
             while !monkey[i].items.is_empty() {
-                // println!("Monkey {}:", monkey[i].monkey_id);
-
                 let init_worry: i64 = match monkey[i].items.pop_front() {
                     None => {
                         panic!("attempted to inspect on monkey without items: {}", monkey[i]);
                     }
                     Some(x) => { x }
                 } as i64;
-                // println!(" Monkey inspects an item with worry level of {init_worry}.");
                 monkey[i].items_inspected +=1;
-
                 let mut new_worry = match monkey[i].oper_type {
                     Operation::Mult => { init_worry * monkey[i].oper_amount }
                     Operation::Add => { init_worry + monkey[i].oper_amount }
@@ -324,45 +275,23 @@ fn part2() -> String {
                     }
                 } ;
                 new_worry = new_worry % mod_base;
-                // println!("    Worry level is {} to {}.",
-                //          match monkey[i].oper_type {
-                //              Operation::Mult => { format!("multiplied by {}", monkey[i].oper_amount) }
-                //              Operation::Add => { format!("increased by {}", monkey[i].oper_amount) }
-                //              Operation::Square => { format!("multiplied by itself") }
-                //              Operation::None => { panic!("shouldn't have Operation::None, {}", monkey[i]) }
-                //          }, new_worry);
-
-             //   new_worry = new_worry / 3;
-                // println!("    Monkey gets bored with item. Worry level is divided by 3 to {new_worry}.");
                 if new_worry % monkey[i].test_divider != 0 {
-                    // println!("    Current worry level is not divisible by {} (false)", monkey[i].test_divider);
-                    // println!("    Item with worry level {new_worry} is thrown to monkey {}.",monkey[i].true_monkey);
                     let t_index = monkey[i].false_monkey;
                     monkey[t_index].items.push_back(new_worry);
                 } else {
                     let t_index = monkey[i].true_monkey;
-                    // println!("    Current worry level is divisible by {} (true).", monkey[i].test_divider);
                     monkey[t_index].items.push_back(new_worry);
                 }
             }
         }
-        // if check_rounds.contains(&round) {
-        //     println!("== After round {round} ==");
-        //     for i in 0..monkey.len() {
-        //         println!("Monkey {i} inspected items {} times.", monkey[i].items_inspected);
-        //     }
-        //     println!();
-        // }
     }
     let mut i_vec = Vec::new();
     for i in 0..monkey.len() {
-        //    println!("{}", monkey[i]);
         i_vec.push(monkey[i].items_inspected);
     }
     i_vec.sort();
     i_vec.reverse();
     let answer2_i = i_vec[0] * i_vec[1];
-    // println!("{:?}",i_vec);
 
     let mut answer2 = answer2_i.to_string();
     return answer2;
