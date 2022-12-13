bd11858a8993b8688320a22e81bc9cba22c2b325
dnabre <alexander@happybunnylabs.com>
2022-12-13 10:55:32 -0700

    Day 13 first plan scrapped need to parse into nested lists
    asuka -> langley

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use std::fmt;
 use std::fs;
 use std::time::Instant;
 use parse_display::FromStr;
+use aho_corasick::AhoCorasick;
 
 /*
     Advent of Code 2022: Day 13
@@ -30,7 +31,7 @@ const PART1_INPUT_FILENAME: &str = "data/day13/part1_input.txt";
 const PART2_TEST_FILENAME: &str = "data/day13/part2_test.txt";
 const PART2_INPUT_FILENAME: &str = "data/day13/part2_input.txt";
 
-const TEST: bool = false;
+const TEST: bool = true;
 
 fn main() {
     print!("Advent of Code 2022, Day ");
@@ -62,6 +63,34 @@ fn main() {
 }
 
 
+fn parse_line(lline: &mut String) -> Vec<i32> {
+    let mut l_vec: Vec<i32> = Vec::new();
+    let mut sb: String = String::from("0");
+
+    let mut ch: char;
+    for ch in lline.chars() {
+        if ch == '[' { continue; }
+        if ch == ']' { continue; }
+        if ch.is_digit(10) {
+            sb.push(ch);
+        }
+        if ch == ',' {
+            if sb.len() > 1 {
+                let n: i32 = sb.parse().unwrap();
+                l_vec.push(n);
+                sb = String::from("0");
+            }
+        }
+    }
+    if sb.len() > 1 {
+        let n: i32 = sb.parse().unwrap();
+        l_vec.push(n);
+        sb = String::from("0");
+    }
+    return l_vec;
+}
+
+
 
 fn part1() -> String {
     let p1_file = match TEST {
@@ -77,7 +106,67 @@ fn part1() -> String {
         println!("\t read {} lines from {}", l_num, p1_file);
     }
 
-    let mut answer1 = String::new();
+    let mut left: Vec<String> = Vec::new();
+    let mut right: Vec<String> = Vec::new();
+
+    println!("{}", lines[0]);
+    println!("{}", lines[1]);
+
+    for i in 0..lines.len() {
+        let mut ln = lines[i].to_string();
+        if left.len() == right.len() {
+            if ln.len() == 0 {
+                continue;
+            } else {
+                left.push(ln.trim().parse().unwrap());
+                continue;
+            }
+        }
+        if left.len() > right.len() {
+            right.push(ln.trim().parse().unwrap());
+            continue;
+        }
+        panic!("more right lists than left lists, input {}", ln);
+    }
+    assert_eq!(left.len(), right.len());
+    let left = left;
+    let right = right;
+    let pair_count = left.len();
+    println!("number of pairs: {pair_count}");
+
+    let mut lline = left[0].clone();
+    let mut rline = right[0].clone();
+
+    for i in 0..pair_count {
+        println!("== Pair {:2>} ==", i+1);
+        let mut s_left = left[i].clone();
+        let mut s_right = right[i].clone();
+        let mut l_vec = parse_line(&mut s_left);
+        let mut r_vec = parse_line(&mut s_right);
+
+        let check:bool = check_vecs(l_vec.clone(), r_vec.clone());
+
+
+        // println!("\tCompare {} vs {}", lline, rline);
+        // println!("\t\"{}\" -> {:?}", s_left, l_vec);
+        // println!("\t\"{}\" -> {:?}", s_right, r_vec);
+         println!("\t  check: {}", match check {
+            true => {"Ok"}
+            false => {"bad"}
+        });
+        println!("--------------");
+    }
+
+
+
+
+
+
+
+    let mut indices: Vec<i32> = Vec::new();
+    let sum: i32 = indices.iter().sum();
+
+    let mut answer1 = sum.to_string();
     return answer1;
 }
 
