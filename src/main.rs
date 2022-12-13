62816473077f2fe533eb0a1fe86b63a05b1024aa
matthew j. barrick <alexander@happybunnylabs.com>
2022-12-12 22:02:36 -0700

    Day 12, part 1 working, part 2 almost working.

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ use parse_display::FromStr;
 
 /*
     Advent of Code 2022: Day 12
-        part1 answer:
+        part1 answer: 383
         part2 answer:
 
  */
 
 
-const TEST_ANSWER: (i32, i32) = (0, 0);
-const INPUT_ANSWER: (i32, i32) = (0, 0);
+const TEST_ANSWER: (i32, i32) = (31, 0);
+const INPUT_ANSWER: (i32, i32) = (383, 0);
 
 
 const PART1_TEST_FILENAME: &str = "data/day12/part1_test.txt";
@@ -41,11 +41,11 @@ fn main() {
     let duration1 = start1.elapsed();
 
     println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
-    // if TEST {
-    //     assert_eq!(answer1, TEST_ANSWER.0.to_string());
-    // } else {
-    //     assert_eq!(answer1, INPUT_ANSWER.0.to_string());
-    // }
+    if TEST {
+        assert_eq!(answer1, TEST_ANSWER.0.to_string());
+    } else {
+        assert_eq!(answer1, INPUT_ANSWER.0.to_string());
+    }
 
     let start2 = Instant::now();
     let answer2 = part2();
@@ -61,6 +61,77 @@ fn main() {
     println!("----------\ndone");
 }
 
+
+
+fn print_grid(grid: &HashMap<(i16, i16), char>, (width,height):(i16, i16)) {
+    println!("grid:    wxh:: {width}x{height})");
+     for h in 0..height{
+        print!("\t");
+        for w in 0..width {
+            let ch = grid.get( &(w as i16, h as i16) ).unwrap();
+            print!("{}", ch);
+        }
+         println!();
+    }
+    println!("---------");
+
+
+}
+fn bfs(grid: &HashMap<(i16, i16), char>, start: (i16, i16), end: (i16, i16)) -> i32 {
+    // this might be closer to an A* than BFS, but I'm basing it on BFS
+    let mut visited: HashSet<(i16,i16)> = HashSet::new();
+    let mut search_queue:VecDeque<(i16,i16)> = VecDeque::new();
+    // delta from x,y for possible neighbors,
+    //                          left , right,  up  ,  down
+    let deltas = vec![(-1,0), (1,0), (0,1), (0, -1)];
+    // since we're using a HashMap<(x,y),char> neighbors off the edge will just not be found.
+    let mut distance_from_start:HashMap<(i16, i16),i32> = HashMap::new();
+    distance_from_start.insert(start,0);
+    visited.insert(start);
+    search_queue.push_back(start);
+    while search_queue.len() > 0 {
+        let (x,y) = search_queue.pop_front().unwrap();
+     //   println!("visitng ({x},{y}):");
+        let mut neighbors:Vec<(i16,i16)> = Vec::new();
+
+        for (dx,dy) in &deltas {
+            let new_x = x +dx;
+            let new_y = y + dy;
+
+            if !visited.contains(&(new_x,new_y)) {
+                if grid.contains_key(&(new_x,new_y)) {
+                    neighbors.push( (new_x,new_y ));
+                }
+            }
+        }
+
+        let d_c_c = *grid.get(&(x,y)).unwrap() ;
+        let c_c = *grid.get(&(x,y)).unwrap() as i16;
+
+    //    println!("visitng ({x},{y}), grid: {}, height: {}",d_c_c,c_c );
+    //    println!("found {} viable neighbors", neighbors.len());
+        for (n_x,n_y) in neighbors {
+
+            let d_n_c= *grid.get(&(n_x,n_y)).unwrap(); // we've check above if this exists
+            let n_c = *grid.get(&(n_x,n_y)).unwrap() as i16; // we've check above if this exists
+    //        println!("\t neighbor ({n_x},{n_y}) in grid is: {} (height = {})", d_n_c, n_c);
+
+            if n_c - c_c  <= 1 {
+                //height of neighbor above current square isn't too high
+                search_queue.push_back((n_x,n_y));
+                visited.insert((n_x,n_y));
+                distance_from_start.insert((n_x,n_y),
+                                           distance_from_start.get(&(x,y)).unwrap() + 1);
+
+                if (n_x, n_y) == end {
+                    return *distance_from_start.get(&(n_x,n_y)).unwrap()  as i32
+                }
+            }
+        }
+  }
+    return -1;
+}
+
 fn part1() -> String {
     let p1_file = match TEST {
         true => PART1_TEST_FILENAME,
@@ -74,10 +145,42 @@ fn part1() -> String {
     if TEST {
         println!("\t read {} lines from {}", l_num, p1_file);
     }
+    let width = lines[0].len();
+    let height = lines.len();
+    println!("input wxh = {width}x{height}");
+    let mut grid: HashMap<(i16,i16),char> = HashMap::new();
 
-    let mut answer1 = String::new();
+    let size = (width as i16, height as i16);
+    let mut start: (i16,i16) = (-1,-1);
+    let mut end: (i16,i16) = (-1,-1);
+
+
+
+    for h in 0..height {
+        let row:Vec<char> = lines[h].chars().collect();
+        for w in 0..width {
+            let ch = row[w];
+            grid.insert((w as i16,h as i16),ch);
+            if ch=='S' {start = (w as i16, h as i16);}
+            if ch=='E' {end = (w as i16, h as i16);}
+        }
+    }
+    // override grid S and E with min and max height accordingly
+    grid.insert(start, 'a');
+    grid.insert(end, 'z');
+
+    println!("grid:\n {:?} \n S={:?}, E={:?}", grid, start, end);
+    print_grid(&grid,size);
+
+    let aa1 =  bfs(&grid, start, end);
+    println!("{:?}",aa1);
+
+    let mut answer1 = aa1.to_string();
     return answer1;
 }
+
+
+
 
 fn part2() -> String {
     let p2_file = match TEST {
@@ -93,7 +196,47 @@ fn part2() -> String {
     if TEST {
         println!("\t read {} lines from {}", l_num, p2_file);
     }
+    let width = lines[0].len();
+    let height = lines.len();
+    println!("input wxh = {width}x{height}");
+    let mut grid: HashMap<(i16,i16),char> = HashMap::new();
 
-    let mut answer2 = String::new();
+    let size = (width as i16, height as i16);
+    let mut start: (i16,i16) = (-1,-1);
+    let mut end: (i16,i16) = (-1,-1);
+
+
+
+    for h in 0..height {
+        let row:Vec<char> = lines[h].chars().collect();
+        for w in 0..width {
+            let ch = row[w];
+            grid.insert((w as i16,h as i16),ch);
+            if ch=='S' {start = (w as i16, h as i16);}
+            if ch=='E' {end = (w as i16, h as i16);}
+        }
+    }
+    // override grid S and E with min and max height accordingly
+    grid.insert(start, 'a');
+    grid.insert(end, 'z');
+
+    println!("grid:\n {:?} \n S={:?}, E={:?}", grid, start, end);
+    print_grid(&grid,size);
+
+    let mut paths:Vec<i32> = Vec::new();
+    for ((s_x,s_y),ch) in grid.iter() {
+     //   println!("({s_x},{s_y}) : {ch}");
+        if *ch == 'a' {
+            let d = bfs(&grid, (*s_x, *s_y), end);
+            paths.push(d);
+        }
+
+
+    }
+    println!("{:?}", paths);
+
+    let aa2 =  paths.iter().min().unwrap();
+    println!("{:?}",aa1);
+    let mut answer2 = aa2.to_string();;
     return answer2;
 }
