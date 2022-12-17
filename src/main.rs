f93408ef5127aed1bb7bd27b68d9de149844decd
dnabre <alexander@happybunnylabs.com>
2022-12-17 09:55:23 -0700

    day 17
    part 1 wrong answer in input (right on test)

--- src/main.rs
+++ src/main.rs
@@ -4,89 +4,229 @@
 #![allow(unused_mut)]
 
 
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::collections::HashSet;
-use std::fmt;
+use std::{cmp, fmt};
+use std::fmt::Formatter;
 use std::fs;
-use std::iter;
-use std::ops::{Add, Index};
-use std::thread::current;
 use std::time::Instant;
 
 use parse_display::FromStr;
 
 
-
 /*
-    Advent of Code 2022: Day 07
-        part1 answer: 1581595
-        part2 answer: 1544176
+    Advent of Code 2022: Day 17
+        part1 answer:
+        part2 answer:
+
+        part1 -> 3158 is too high
 
  */
 
-const TEST_ANSWER: (u32, u32) = (95437, 24933642);
-const INPUT_ANSWER: (u32, u32) = (1581595, 1544176);
+const TEST_ANSWER: (i64, i64) = (3068, 3059);
+const INPUT_ANSWER: (i64, i64) = (3158, 1500874635587);
 
 
-const PART1_TEST_FILENAME: &str = "data/day07/part1_test.txt";
-const PART1_TEST2_FILENAME: &str = "data/day07/part1_test_2.txt";
-const PART1_INPUT_FILENAME: &str = "data/day07/part1_input.txt";
+const PART1_TEST_FILENAME: &str = "data/day17/part1_test.txt";
+const PART1_INPUT_FILENAME: &str = "data/day17/part1_input.txt";
 
-const PART2_TEST_FILENAME: &str = "data/day07/part2_test.txt";
-const PART2_INPUT_FILENAME: &str = "data/day07/part2_input.txt";
-
-const PART1_DIR_SIZE_CAP: u32 = 100_000;
-
-const PART2_TOTAL_SPACE: u32 = 70_000_000;
-const PART2_TARGET_UNUSED_SPACE: u32 = 30_000_000;
-
+const PART2_TEST_FILENAME: &str = "data/day17/part2_test.txt";
+const PART2_INPUT_FILENAME: &str = "data/day17/part2_input.txt";
 
 const TEST: bool = false;
 
 fn main() {
     print!("Advent of Code 2022, Day ");
-    println!("07");                           // insert Day
+    println!("17");                           // insert Day
 
 
-    // test();
     let start1 = Instant::now();
     let answer1 = part1();
     let duration1 = start1.elapsed();
+
     println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
-    if TEST {
-        assert_eq!(answer1, TEST_ANSWER.0.to_string());
-    } else {
-        assert_eq!(answer1, INPUT_ANSWER.0.to_string());
-    }
+    //
+    // if TEST {
+    //     assert_eq!(answer1, TEST_ANSWER.0.to_string());
+    // } else {
+    //     assert_eq!(answer1, INPUT_ANSWER.0.to_string());
+    // }
+    //
+    // let start2 = Instant::now();
+    // let answer2 = part2();
+    // let duration2 = start2.elapsed();
+    //
+    // println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
 
-    let start2 = Instant::now();
-    let answer2 = part2();
-    let duration2 = start2.elapsed();
-    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
-    if TEST {
-        assert_eq!(answer2, TEST_ANSWER.1.to_string());
-    } else {
-        assert_eq!(answer2, INPUT_ANSWER.1.to_string());
-    }
-
+    // if TEST {
+    //     assert_eq!(answer2, TEST_ANSWER.1.to_string());
+    // } else {
+    //     assert_eq!(answer2, INPUT_ANSWER.1.to_string());
+    // }
 
     println!("----------\ndone");
 }
 
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+enum Block {
+    HLine,
+    Cross,
+    Corner,
+    VLine,
+    Square,
+}
 
-#[derive(FromStr, Debug)]
-#[display("{size} {name}")]
-struct File {
-    name: String,
-    size: u32,
+impl fmt::Display for Block {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            Block::HLine => { write!(f, "hort-bar") }
+            Block::Cross => { write!(f, "cross") }
+            Block::Corner => { write!(f, "corner") }
+            Block::VLine => { write!(f, "vert-bar") }
+            Block::Square => { write!(f, "square") }
+        }
+    }
 }
 
 
-#[derive(FromStr, Debug)]
-#[display("dir {name}")]
-struct Dir {
-    name: String,
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Direction::Up => write!(f, "🠕"),
+            Direction::Down => write!(f, "🠗"),
+            Direction::Left => write!(f, "🠔"),
+            Direction::Right => write!(f, "➝"),
+        }
+    }
+}
+
+
+fn can_move(field: &[u8], r: Block, dir: Direction, row: usize, col: usize) -> bool {
+    match dir {
+        Direction::Up => {
+            unreachable!()
+        }
+
+        Direction::Down => {
+            0 < row
+                && match r {
+                Block::HLine => {
+                    field[row - 1] & (0xF << (7 - col - 3)) == 0
+                }
+                Block::Cross => {
+                    field[row - 1] & (1 << (7 - col - 1)) | field[row] &
+                        (0x5 << (7 - col - 2)) == 0
+                }
+                Block::Corner => {
+                    field[row - 1] & (0x7 << (7 - col - 2)) == 0
+                }
+                Block::VLine => {
+                    field[row - 1] & (1 << (7 - col)) == 0
+                }
+                Block::Square => {
+                    field[row - 1] & (0x3 << (7 - col - 1)) == 0
+                }
+            }
+        }
+        Direction::Left => {
+            0 < col &&
+                match r {
+                    Block::HLine => {
+                        field[row] & (1 << (7 - col + 1)) == 0
+                    }
+                    Block::Cross => {
+                        (field[row] | field[row + 2])
+                            & (1 << (7 - col + 1))
+                            | field[row + 1]
+                            & (1 << (7 - col + 1))
+                            == 0
+                    }
+                    Block::Corner => {
+                        field[row] & (1 << (7 - col + 1)) |
+                            (field[row + 1] | field[row + 2]) &
+                                (1 << (7 - col - 1)) == 0
+                    }
+                    Block::VLine => {
+                        (field[row] | field[row + 1] | field[row + 2] | field[row + 3])
+                            & (1 << (7 - col + 1))
+                            == 0
+                    }
+                    Block::Square => {
+                        (field[row] | field[row + 1]) & (1 << (7 - col + 1)) == 0
+                    }
+                }
+        }
+        Direction::Right => {
+            match r {
+                Block::HLine => col + 4 < 7 && field[row] & (1 << (7 - col - 4)) == 0,
+                Block::Cross => {
+                    col + 3 < 7
+                        && (field[row] | field[row + 2]) & (1 << (7 - col - 2))
+                        | field[row + 1] & (1 << (7 - col - 3))
+                        == 0
+                }
+                Block::Corner => {
+                    col + 3 < 7
+                        && (field[row] | field[row + 1] | field[row + 2]) & (1 << (7 - col - 3)) == 0
+                }
+                Block::VLine => {
+                    col + 1 < 7
+                        && (field[row] | field[row + 1] | field[row + 2] | field[row + 3])
+                        & (1 << (7 - col - 1))
+                        == 0
+                }
+                Block::Square => {
+                    col + 2 < 7 && (field[row] | field[row + 1]) & (1 << (7 - col - 2)) == 0
+                }
+            }
+        }
+    }
+}
+
+
+fn freeze(field:&mut [u8], r: Block, row: usize, col: usize) -> usize {
+    match r {
+        Block::HLine => {
+            field[row] |= 0xF << (7 - col - 3);
+            row
+        }
+        Block::Cross => {
+            field[row] |= 1 << (7 - col - 1);
+            field[row + 1] |= 0x7 << (7 - col - 2);
+            field[row + 2] |= 1 << (7 - col - 1);
+            row + 2
+        }
+        Block::Corner => {
+            field[row] |= 0x7 << (7 - col - 2);
+            field[row + 1] |= 1 << (7 - col - 2);
+            field[row + 2] |= 1 << (7 - col - 2);
+            row + 2
+        }
+        Block::VLine => {
+            field[row] |= 1 << (7 - col);
+            field[row + 1] |= 1 << (7 - col);
+            field[row + 2] |= 1 << (7 - col);
+            field[row + 3] |= 1 << (7 - col);
+            row + 3
+        }
+        Block::Square => {
+            field[row] |= 3 << (7 - col - 1);
+            field[row + 1] |= 3 << (7 - col - 1);
+            row + 1
+        }
+    }
+}
+
+
+
 
 
 fn part1() -> String {
@@ -98,185 +238,101 @@ fn part1() -> String {
         fs::read_to_string(p1_file).expect(&*format!("error opening file {}", p1_file));
     let mut lines: Vec<&str> = data1_s.trim().split("\n").collect();
     let l_num = lines.len();
-    if TEST {
+    if !TEST {
         println!("\t read {} lines from {}", l_num, p1_file);
-    }
-
-    for i in 0..lines.len() {
-        lines[i] = lines[i].trim();
-    }
-
-
-    let (mut dir_size, mut directory_set): (HashMap<String, u32>, HashSet<String>)
-        = parse_into_directories(&mut lines);
-
-
-    let cum_dir_sizes: HashMap<String, u32> =
-        get_cumulative_directory_sizes(&directory_set, &dir_size);
-
-    let mut total_size: u32 = 0;
-
-    //println!("\n directory list:");
-    for d in &directory_set {
-        let size = cum_dir_sizes.get(d);
-        match size {
-            None => {
-                println!("\t{d:<20}  \t size: (unknown)");
-                panic!("directory {d} should have known size");
-            }
-            Some(&s) => {
-                if s < PART1_DIR_SIZE_CAP {
-                    total_size += s;
-                }
-                //        println!("\t{d:<20}  \t size: {s}");
-            }
+        if l_num == 1 {
+            println!("\t\t line read has length: {}", lines[0].len());
         }
     }
 
+    let mut field = vec![0u8; 10];
+    let mut highest_row = 0;
+    let jets = lines[0].trim_end().as_bytes();
+    let mut jet_index = 0;
 
-    println!("directory entries: {}", directory_set.len());
-    let mut answer1 = total_size.to_string();
-    return answer1;
-}
 
+    let last_turn: usize = 50;
 
-// ctor_set):(HashMap<String,u32>,HashSet<String> )
-
-fn parse_into_directories(lines: &mut Vec<&str>) -> (HashMap<String, u32>, HashSet<String>) {
-    let mut dir_size: HashMap<String, u32> = HashMap::new();
-    let mut directory_set: HashSet<String> = HashSet::new();
-
-    let mut cwd = String::from("/");
-    directory_set.insert(cwd.clone());
-    dir_size.insert(cwd.clone(), 0);
-
-    let mut line_count = 0;
-    // Assume first command will be "cd /"
-    for line in lines {
-        line_count += 1;
-        //       println!("cwd: {cwd} \t line: {line} \t\t l_num: {line_count}");
-        if line.starts_with("$") {
-            //command
-            if line.starts_with("$ ls") {
-                //             println!("listing  : {cwd}");
-            } else {  // change directory command
-                let mut debug = false;
-                let (_, dir) = line.split_once("cd ").unwrap();
-                //    println!("cd from {cwd}, {dir}");
-                match dir {
-                    "/" => {
-                        cwd = String::from("/");
-                    }
-                    ".." => {
-                        if cwd.ne("/") {
-                            // let p_cwd = dir_up(&mut cwd);
-                            // cwd = p_cwd;
-                            //
-                            cwd = dir_up(&cwd);
-                        }
-                    }
-                    d_name => {
-                        if debug { println!("\t cd __ branch, cwd: {cwd}, d_name: {d_name}") };
-                        //    if cwd.ne("/") { cwd.push('/'); }
-                        cwd.push_str(d_name);
-                        if debug { println!("push_str d_name ({d_name}) onto cwd, resulting in {cwd}") };
-                        cwd.push('/');
-                        if debug { println!("pushed slash onto cwd, resulting {cwd}") };
-                        let rr = directory_set.insert(cwd.clone());
-                        if debug { println!("inserted clone of cwd into directory_set, result {}", rr) };
-                        let rr2 = dir_size.insert(cwd.clone(), 0);
-                        if debug { println!("insert cwd = 0 , {cwd} into dir_size, result {:?}", rr2) };
-                    }
-                }
-                //          println!("moved to {cwd}");
-            }
-            //end command
+    let mut more_debug;
+    'block: for i in 0..last_turn {
+        println!("top of block loop, turn {i}, {highest_row}");
+        if i ==  27 {
+            more_debug = true;
         } else {
-            //listing
-
-
-
-            if line.starts_with("dir") {
-                let d: Dir = line.parse().unwrap();
-                //        println!("list  dir: {} \t -:- {cwd} ", d.name);
-            } else {
-                let mut c_size = match dir_size.get(&cwd) {
-                    None => { 0 }
-                    Some(&x) => { x }
-                };
-                let f: File = line.parse().unwrap();
-                //      println!("\t File: {cwd} <|> {} \t{}", f.name,f.size);
-                c_size = c_size + f.size;
-                let r = dir_size.insert(cwd.clone(), c_size);
-            }
+            more_debug = false;
         }
-    }
 
-    return (dir_size, directory_set);
-}
-
-
-fn get_cumulative_directory_sizes(dirs_set: &HashSet<String>, dir_size: &HashMap<String, u32>) -> HashMap<String, u32> {
-    let mut cum_size_map: HashMap<String, u32> = HashMap::new();
-
-    for dir in dirs_set {
-        let cwd = dir.clone();
-        let mut size: u32 = 0;
-        // println!("scanning for subdirs of {cwd}");
-        for sub_dir in dirs_set {
-            let sd_size = match dir_size.get(sub_dir) {
-                None => {
-                    panic!("can't find size for {sub_dir}");
+        let mut bottom = highest_row + 3;
+        for _ in field.len()..bottom + 4 {
+            field.push(0);
+        }
+        let mut left = 2;
+        let r = match i % 5 {
+            0 => Block::HLine,
+            1 => Block::Cross,
+            2 => Block::Corner,
+            3 => Block::VLine,
+            4 => Block::Square,
+            _ => unreachable!(),
+        };
+        if more_debug {
+            println!("\tblock, r: {:?}", r);
+            println!("\tfield: {:?}", field);
+            println!("\tbottom: {bottom}");
+            println!("\thigh_row: {highest_row}");
+            println!("\tjet_index: {jet_index}");
+        }
+        bottom -= 3;
+        for _ in 0..3 {
+            match jets[jet_index % jets.len()] {
+                60 => { //left
+                    if can_move(&field, r, Direction::Left, bottom, left) {
+                        left -= 1;
+                    }
                 }
-                Some(x) => { x }
-            };
-            // println!("\t subdir: {sub_dir:<20} \t size: {sd_size} ");
-            if sub_dir.starts_with(&cwd) {
-                let new_size = sd_size + size;
-                // println!("\t\t {sub_dir} is inside {dir}, adding it's size: {} + {} => {}", sd_size, size, new_size);
-                size = new_size;
+                62 => { //right
+                    if can_move(&field, r, Direction::Right, bottom, left) {
+                        left += 1;
+                    }
+                }
+                _ => unreachable!(),
+            }
+            jet_index += 1;
+        }
+
+        loop {
+            match jets[jet_index % jets.len()] {
+                60 => { //left
+                    if can_move(&field, r, Direction::Left, bottom, left) {
+                        left -= 1
+                    }
+                }
+                62 => { //right
+                    if can_move(&field, r, Direction::Right, bottom, left) {
+                        left += 1
+                    }
+                }
+                _ => unreachable!(),
+            }
+            jet_index += 1;
+
+            let dir = Direction::Down;
+            if !can_move(&field, r, dir, bottom, left) {
+                let row = freeze(&mut field, r, bottom, left);
+           //     highest_row = cmp::max(row + 1, highest_row);
+                if row +1 > highest_row {
+                    highest_row = row + 1
+                }
+
+                continue 'block;
+            } else {
+                bottom -= 1;
             }
         }
-        cum_size_map.insert(cwd, size);
     }
 
-    return cum_size_map;
-}
-
-
-fn last_char_is(s: &String, ch: char) -> bool {
-    let c = s.chars().last().unwrap();
-    if c == ch {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-/*
- fn dir_up(cwd: & String) -> String {
-    let mut p_cwd = cwd.clone();
-    p_cwd.pop();
-    let (left, _) = p_cwd.rsplit_once("/").unwrap();
-    if left.is_empty() {
-        p_cwd = String::from("/");
-    } else {
-        p_cwd = String::from(left);
-    }
-    return p_cwd
-}
-*/
-fn dir_up(cwd: &String) -> String {
-    let mut p_cwd = cwd.clone();
-    p_cwd.pop();
-    let (left, _) = p_cwd.rsplit_once("/").unwrap();
-    if left.is_empty() {
-        p_cwd = String::from("/");
-    } else {
-        p_cwd = String::from(left);
-    }
-    p_cwd.push('/');
-    return p_cwd;
+    let answer1 = highest_row as u32;
+    return answer1.to_string();
 }
 
 fn part2() -> String {
@@ -287,48 +343,12 @@ fn part2() -> String {
     let data2_s =
         fs::read_to_string(p2_file).expect(&*format!("error opening file {}", p2_file));
 
-
     let mut lines: Vec<&str> = data2_s.trim().split("\n").collect();
     let l_num = lines.len();
     if TEST {
         println!("\t read {} lines from {}", l_num, p2_file);
     }
 
-    for i in 0..lines.len() {
-        lines[i] = lines[i].trim();
-    }
-
-
-    let (mut dir_size, mut directory_set): (HashMap<String, u32>, HashSet<String>)
-        = parse_into_directories(&mut lines);
-
-
-    let cum_dir_sizes: HashMap<String, u32> =
-        get_cumulative_directory_sizes(&directory_set, &dir_size);
-    let mut root_used = cum_dir_sizes.get("/").unwrap();
-    let mut free_space = PART2_TOTAL_SPACE - root_used;
-    let mut need_to_free = PART2_TARGET_UNUSED_SPACE - free_space;
-
-
-    let mut dir_size_totals: Vec<&u32> = cum_dir_sizes.values().collect();
-    //   println!("{:?}", dir_size_totals);
-
-
-    let mut size_values: Vec<u32> = dir_size_totals.iter().map(|x| **x).collect();
-    size_values.sort();
-
-    let mut to_free = 0;
-
-    let mut to_free = 0;
-    for i in 0..size_values.len() {
-        let v = size_values[i];
-        if v >= need_to_free {
-            to_free = v;
-            break;
-        }
-    }
-    // target answer 1544176
-
-    let mut answer2 = to_free.to_string();
+    let mut answer2 = String::new();
     return answer2;
 }
