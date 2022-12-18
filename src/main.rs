4097014ec1e12c85f92a746938e34680823149c2
dnabre <alexander@happybunnylabs.com>
2022-12-18 13:57:58 -0700

    Day 18, part 1 finshed for part 2 need to do flood fill 3d array of enums created.

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,24 @@
 use std::collections::{HashMap, VecDeque};
 use std::collections::HashSet;
 use std::{cmp, fmt};
-use std::fmt::Formatter;
+use std::fmt::{Display, Formatter};
 use std::fs;
 use std::time::{Instant,Duration};
 
+use ndarray::{Array, Array2, Array3, ArrayBase, OwnedRepr, Dim};
 use parse_display::FromStr;
 
 
 /*
     Advent of Code 2022: Day 18
-        part1 answer:
+        part1 answer: 4282
         part2 answer:
 
-
+part2,  4036 is too high
  */
 
-const TEST_ANSWER: (i64, i64) = (0, 0);
-const INPUT_ANSWER: (i64, i64) = (0, 0);
-
+const TEST_ANSWER: (i64, i64) = (64, 58);
+const INPUT_ANSWER: (i64, i64) = (4282, 0);
 
 const PART1_TEST_FILENAME: &str = "data/day18/part1_test.txt";
 const PART1_INPUT_FILENAME: &str = "data/day18/part1_input.txt";
@@ -45,17 +45,17 @@ fn main() {
 
     println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
     //
-    // if TEST {
-    //     assert_eq!(answer1, TEST_ANSWER.0.to_string());
-    // } else {
-    //     assert_eq!(answer1, INPUT_ANSWER.0.to_string());
-    // }
-    //
-    // let start2 = Instant::now();
-    // let answer2 = part2();
-    // let duration2 = start2.elapsed();
-    //
-    // println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
+    if TEST {
+        assert_eq!(answer1, TEST_ANSWER.0.to_string());
+    } else {
+        assert_eq!(answer1, INPUT_ANSWER.0.to_string());
+    }
+
+    let start2 = Instant::now();
+    let answer2 = part2();
+    let duration2 = start2.elapsed();
+
+    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
 
     // if TEST {
     //     assert_eq!(answer2, TEST_ANSWER.1.to_string());
@@ -66,6 +66,65 @@ fn main() {
     println!("----------\ndone");
 }
 
+#[cfg(windows)]
+const D_LINE_ENDING: &'static str = "\r\n\r\n";
+#[cfg(not(windows))]
+const D_LINE_ENDING: &'static str = "\n\n";
+
+#[cfg(windows)]
+const LINE_ENDING: &'static str = "\r\n";
+#[cfg(not(windows))]
+const LINE_ENDING: &'static str = "\n";
+
+const ARRAY_SIZE:usize = 25;
+
+#[derive(FromStr, Debug, Copy, Clone,Eq, Hash)]
+#[display("{x},{y},{z}")]
+struct Point3D {
+    x:i32,
+    y:i32,
+    z:i32
+}
+
+impl PartialEq for Point3D {
+    fn eq(&self, other: &Self) -> bool {
+        (self.x == other.x) && (self.y == other.y) && (self.z == other.z)
+    }
+}
+
+impl Display for Point3D{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f,"<{},{},{}>", self.x,self.y,self.z)
+    }
+}
+#[derive(Debug, Copy, Clone,PartialEq,Eq, Hash)]
+enum Voxel  {
+    Unknown=0,
+    Outside,
+    Inside,
+    Lava
+}
+
+impl Display for Voxel{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Voxel::Lava => {write!(f,"L")}
+            Voxel::Outside => {write!(f,"O")}
+            Voxel::Inside=> {write!(f,"I")}
+            Voxel::Unknown => {write!(f,"?")}
+        }
+    }
+}
+
+impl Default for Voxel {
+    fn default() -> Voxel {
+        return Voxel::Unknown
+    }
+}
+
+
+
+
 fn part1() -> String {
     let p1_file = match TEST {
         true => PART1_TEST_FILENAME,
@@ -75,16 +134,57 @@ fn part1() -> String {
         fs::read_to_string(p1_file).expect(&*format!("error opening file {}", p1_file));
     let mut lines: Vec<&str> = data1_s.trim().split("\n").collect();
     let l_num = lines.len();
-    if !TEST {
+    if TEST {
         println!("\t read {} lines from {}", l_num, p1_file);
         if l_num == 1 {
             println!("\t\t line read has length: {}", lines[0].len());
         }
     }
+    let data1_ss = data1_s.trim();
+    let split_lines:Vec<&str> = data1_ss.split(LINE_ENDING).collect();
+
+    let mut v_points:Vec<Point3D> = Vec::new();
+    for i in 0..split_lines.len() {
+        let pp = split_lines[i].parse::<Point3D>().unwrap();
+        v_points.push(pp);
+    }
+
+    let v_points = v_points.clone();
+
+    let v_points = v_points.clone();
+
+    let mut p_cloud:HashSet<Point3D> = HashSet::new();
+    for p in v_points.clone() {
+        p_cloud.insert(p.clone());
+    }
+    let mut cube_count =0;
+
+   for p in &v_points {
+       let mut sides:i32=0;
+       let mut h_sides:HashSet<Point3D> = HashSet::new();
+
+       let mut pp = Point3D{x:p.x+1, y:p.y, z:p.z};
+       h_sides.insert(pp.clone());
+       let mut pp = Point3D{x:p.x-1, y:p.y, z:p.z};
+       h_sides.insert(pp.clone());
+       let mut pp = Point3D{x:p.x, y:p.y+1, z:p.z};
+       h_sides.insert(pp.clone());
+       let mut pp = Point3D{x:p.x, y:p.y-1, z:p.z};
+       h_sides.insert(pp.clone());
+       let mut pp = Point3D{x:p.x, y:p.y, z:p.z+1};
+       h_sides.insert(pp.clone());
+       let mut pp = Point3D{x:p.x, y:p.y, z:p.z-1};
+       h_sides.insert(pp.clone());
+
+        for h in h_sides {
+            if !p_cloud.contains(&h) {
+                cube_count +=1;
+            }
+        }
+   }
 
 
-
-    let answer1 = String::new();
+    let answer1 = cube_count.to_string();
     return answer1.to_string();
 }
 
@@ -100,7 +200,162 @@ fn part2() -> String {
     let l_num = lines.len();
     if TEST {
         println!("\t read {} lines from {}", l_num, p2_file);
+        if l_num == 1 {
+            println!("\t\t line read has length: {}", lines[0].len());
+        }
     }
+    let data2_ss = data2_s.trim();
+    let split_lines:Vec<&str> = data2_ss.split(LINE_ENDING).collect();
+
+    let mut v_points:Vec<Point3D> = Vec::new();
+    for i in 0..split_lines.len() {
+        let pp = split_lines[i].parse::<Point3D>().unwrap();
+        v_points.push(pp);
+    }
+
+    let v_points = v_points.clone();
+
+    println!("v_points, len={} ", v_points.len());
+
+    let v_points = v_points.clone();
+
+    let  (mut x_min,mut x_max) = (i32::MAX, i32::MIN);
+    let  (mut y_min,mut y_max) = (i32::MAX, i32::MIN);
+    let  (mut z_min,mut z_max) = (i32::MAX, i32::MIN);
+
+    let mut p_cloud:HashSet<Point3D> = HashSet::new();
+    for p in v_points.clone() {
+        x_min = cmp::min(p.x, x_min);
+        y_min = cmp::min(p.y, y_min);
+        z_min = cmp::min(p.z, z_min);
+
+        x_max = cmp::max(p.x, x_max);
+        y_max = cmp::max(p.y, y_max);
+        z_max = cmp::max(p.z, z_max);
+
+        p_cloud.insert(p.clone());
+    }
+    println!("Points span the range <{x_min},{y_min},{z_min}> to <{x_max},{y_max},{z_max}>");
+
+    let mut cube_count =0;
+
+    for p in &v_points {
+        let mut sides:i32=0;
+        let mut h_sides:HashSet<Point3D> = HashSet::new();
+
+        let mut pp = Point3D{x:p.x+1, y:p.y, z:p.z};
+        h_sides.insert(pp.clone());
+        let mut pp = Point3D{x:p.x-1, y:p.y, z:p.z};
+        h_sides.insert(pp.clone());
+        let mut pp = Point3D{x:p.x, y:p.y+1, z:p.z};
+        h_sides.insert(pp.clone());
+        let mut pp = Point3D{x:p.x, y:p.y-1, z:p.z};
+        h_sides.insert(pp.clone());
+        let mut pp = Point3D{x:p.x, y:p.y, z:p.z+1};
+        h_sides.insert(pp.clone());
+        let mut pp = Point3D{x:p.x, y:p.y, z:p.z-1};
+        h_sides.insert(pp.clone());
+
+        for h in h_sides {
+            if !p_cloud.contains(&h) {
+                cube_count +=1;
+            }
+        }
+    }
+    println!("cube_count= {cube_count}");
+    let mut hole_count:i32 = 0;
+    let mut considered_holes:i32 = 0;
+    // v_holes: count of single voxel holes
+    let mut v_holes:HashSet<Point3D> = HashSet::new();
+    for xx in (x_min-1)..=(x_max+1) {
+        for yy in (y_min-1)..=(y_max+1) {
+            for zz in (z_min-1)..=(z_max+1) {
+                let p = Point3D{x:xx,y:yy,z:zz};
+                if p_cloud.contains(&p) {
+                    // point is lava, ignore
+                    continue;
+                }
+                considered_holes +=1;
+                let mut sides =0;
+                // check point on each side, and count how many are lava
+
+                let mut pp = Point3D{x:p.x+1, y:p.y, z:p.z};
+                if p_cloud.contains(&pp) {
+                    sides += 1;
+                }
+                let mut pp = Point3D{x:p.x-1, y:p.y, z:p.z};
+                if p_cloud.contains(&pp) {
+                    sides += 1;
+                }
+                let mut pp = Point3D{x:p.x, y:p.y+1, z:p.z};
+                if p_cloud.contains(&pp) {
+                    sides += 1;
+                }
+                let mut pp = Point3D{x:p.x, y:p.y-1, z:p.z};
+                if p_cloud.contains(&pp) {
+                    sides += 1;
+                }
+                let mut pp = Point3D{x:p.x, y:p.y, z:p.z+1};
+                if p_cloud.contains(&pp) {
+                    sides += 1;
+                }
+                let mut pp = Point3D{x:p.x, y:p.y, z:p.z-1};
+                if p_cloud.contains(&pp) {
+                    sides += 1;
+                }
+                if sides == 6 {
+                    //println!("\tquery point {p} has lava on {} sides", sides);
+                    hole_count +=1;
+                    v_holes.insert(p.clone());
+                }
+            }
+        }
+    }
+
+    let mut previous = v_holes.len();
+
+    let mut longest_dir = cmp::max(x_max, y_max);
+    longest_dir = cmp::max(longest_dir, z_max);
+    longest_dir +=1;
+    let a_size:usize = longest_dir as usize;
+
+    let v = vec![Voxel::Unknown; a_size * a_size * a_size];
+
+    for p in p_cloud {
+        if p.x==0 || p.y==0 || p.z ==0 {
+            println!("near oring point: {p}");
+        }
+    }
+
+
+    let mut grid: [[[Voxel; ARRAY_SIZE]; ARRAY_SIZE]; ARRAY_SIZE] = Default::default();
+
+    // ArrayBase<OwnedRepr<i32>,
+
+ //  let mut grid:ArrayBase<OwnedRepr<Voxel>,Dim<[usize; 3]>> =Array3::from_shape_vec((a_size,a_size,a_size), v).unwrap();
+
+  // let mut grid = Array3::<Voxel>::zeros((a_size, a_size, a_size));
+  //  println!("shape {:?}", grid.shape());
+
+
+
+   // for p in p_cloud {
+   //     grid[[p.x,p.y,p.z]] = Voxel::Lava;
+   // }
+   // for h in v_holes {
+   //     grid[[h.x,h.y,h.z]] = Voxel::Inside;
+   // }
+   //
+   //  for t in 0..a_size {
+   //      println!("accessing [0,0,{t}]");
+   //      let v = grid[[0,0,t]];
+   //  }
+
+
+
+    println!("cube_count: {cube_count}");
+    println!("hole_count: {hole_count}");
+    println!("exposed side count: {}", cube_count -6*hole_count);
 
     let mut answer2 = String::new();
     return answer2;
