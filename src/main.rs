ca9a451e601e4ae1e429666e31dec59661728f9a
matthew j. barrick <alexander@happybunnylabs.com>
2022-12-19 17:47:01 -0700

    Day 19, part 1 working, testing part 2

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,21 @@ use std::cmp::Ordering;
 use std::cmp::Reverse;
 use std::fmt::{Display, Formatter};
 use std::fs;
-
 use std::time::{Instant,Duration};
+use std::ops::Index;
 
 use parse_display::FromStr;
-use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
+
 
 /*
     Advent of Code 2022: Day 19
-        part1 answer: =
+        part1 answer:  1616
         part2 answer:
 
  */
 
-const TEST_ANSWER: (i64, i64) = (0, 0);
-const INPUT_ANSWER: (i64, i64) = (0, 0);
+const TEST_ANSWER: (i64, i64) = (33, 56);
+const INPUT_ANSWER: (i64, i64) = (1616, 0);
 
 const PART1_TEST_FILENAME: &str = "data/day19/part1_test.txt";
 const PART1_INPUT_FILENAME: &str = "data/day19/part1_input.txt";
@@ -45,11 +44,11 @@ fn main() {
     println!("19");                           // insert Day
 
 
-    let start1 = Instant::now();
-    let answer1 = part1();
-    let duration1 = start1.elapsed();
-
-    println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
+    // let start1 = Instant::now();
+    // let answer1 = part1();
+    // let duration1 = start1.elapsed();
+    //
+    // println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
     //
     // if TEST {
     //     assert_eq!(answer1, TEST_ANSWER.0.to_string());
@@ -57,11 +56,11 @@ fn main() {
     //     assert_eq!(answer1, INPUT_ANSWER.0.to_string());
     // }
 
-    // let start2 = Instant::now();
-    // let answer2 = part2();
-    // let duration2 = start2.elapsed();
-    //
-    // println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
+    let start2 = Instant::now();
+    let answer2 = part2();
+    let duration2 = start2.elapsed();
+
+    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
 
     // if TEST {
     //     assert_eq!(answer2, TEST_ANSWER.1.to_string());
@@ -72,23 +71,32 @@ fn main() {
     println!("----------\ndone");
 }
 
+
+#[repr(usize)]
+enum Material {
+    Ore=0,
+    Clay=1,
+    Obsidian=2,
+    Geode=3
+}
+
 #[derive(PartialEq, Debug, Copy, Clone) ]
 struct BluePrint {
     id: u32,
-    ore_robot_cost: (u32,u32,u32,u32),
-    clay_robot_cost: (u32,u32,u32,u32),
-    obsidian_robot_cost: (u32,u32,u32,u32),
-    geode_robot_cost: (u32,u32,u32,u32),
+    ore_robot_cost: [u16;4],
+    clay_robot_cost: [u16;4],
+    obsidian_robot_cost: [u16;4],
+    geode_robot_cost: [u16;4],
 }
 impl BluePrint {
     fn new(b: BluePrintInput) -> Self
     {
         BluePrint{
             id: b.id as u32,
-            ore_robot_cost: (b.ore_r_ore,0,0,0),
-            clay_robot_cost: (b.clay_r_ore,0,0,0),
-            obsidian_robot_cost: (b.ob_r_ore ,b.ob_r_clay,0,0),
-            geode_robot_cost: (b.geo_r_ore,0,b.geo_r_ob,0 )
+            ore_robot_cost: [0,0,0,0],
+            clay_robot_cost: [0,0,0,0],
+            obsidian_robot_cost: [0,0,0,0],
+            geode_robot_cost: [0,0,0,0]
         }
     }
 }
@@ -97,12 +105,12 @@ impl BluePrint {
 #[display("Blueprint {id}: Each ore robot costs {ore_r_ore} ore. Each clay robot costs {clay_r_ore} ore. Each obsidian robot costs {ob_r_ore} ore and {ob_r_clay} clay. Each geode robot costs {geo_r_ore} ore and {geo_r_ob} obsidian.")]
 struct BluePrintInput {
     id:u32,
-   ore_r_ore:u32,
-    clay_r_ore:u32,
-    ob_r_ore: u32,
-    ob_r_clay: u32,
-    geo_r_ore: u32,
-    geo_r_ob :u32
+   ore_r_ore:u16,
+    clay_r_ore:u16,
+    ob_r_ore: u16,
+    ob_r_clay: u16,
+    geo_r_ore: u16,
+    geo_r_ob :u16
 }
 
 impl Default for BluePrintInput {
@@ -119,189 +127,135 @@ impl Default for BluePrintInput {
     }
 }
 
-#[derive(PartialEq, Debug, Copy, Clone) ]
-enum Action {
-    BuildGeodeBot,
-    BuildObsidianBot,
-    BuildClayBot,
-    BuildOreBot
-}
-
 
 #[derive(Hash, Debug, PartialEq, Eq, Copy, Clone, PartialOrd, Ord)]
 struct State {
-    turn: u32,
-    ore: (u32,u32,u32,u32),
-    machine: (u32,u32,u32,u32),
+    turn: u16,
+    ore: [u16;4],
+    machine: [u16;4],
 }
 impl Default for State {
     fn default() -> State {
         State {
-            turn: 1,
-            ore: (0, 0, 0, 0),
-            machine: (1, 0, 0, 0),
+            turn: 0,
+            ore: [0, 0, 0, 0],
+            machine: [1, 0, 0, 0],
         }
     }
+}
+impl  State{
+
 }
 
 impl Display for State {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f,"State, t: {:>3} ore: {:>3},{:>3},{:>3},{:>3}, machine: {:>3},{:>3},{:>3},{:>3}",
+        write!(f,"State, t: {:>3} ore: [{:>3},{:>3},{:>3},{:>3}] machine: [{:>3},{:>3},{:>3},{:>3}]",
             self.turn,
-               self.ore.0, self.ore.1, self.ore.2, self.ore.3,
-                self.machine.0,self.machine.1,self.machine.2,self.machine.3
+            self.ore[0],self.ore[1],self.ore[2], self.ore[3],
+              self.machine[0],self.machine[1],self.machine[2], self.machine[3]
         )
     }
 }
-impl State {
-    fn tick(mut self)->State {
-        let mut new_state = self.clone();
-        new_state.turn +=1 ;
-        new_state.ore = tuple_add(self.ore, self.machine);
-        return new_state;
-    }
-
-    fn tick_and_build(&self,bp:&BluePrint, action:&Action) -> State {
-        let mut new_state = self.clone();
-        new_state.turn +=1 ;
-        new_state.ore = tuple_add(self.ore, self.machine);
-        match action {
-            Action::BuildGeodeBot => {
-                assert!(self.can_build(bp,action,));
-                new_state.ore = tuple_sub(self.ore, bp.geode_robot_cost);
-                new_state.machine.3 +=1;
-            }
-            Action::BuildObsidianBot => {
-                assert!(self.can_build(bp,action,));
-                new_state.ore = tuple_sub(self.ore, bp.obsidian_robot_cost);
-                new_state.machine.2 +=1;
-            }
-            Action::BuildClayBot => {
-                assert!(self.can_build(bp,action,));
-                new_state.ore = tuple_sub(self.ore, bp.clay_robot_cost);
-                new_state.machine.1 +=1;
-            }
-            Action::BuildOreBot => {
-                assert!(self.can_build(bp,action,));
-                new_state.ore = tuple_sub(self.ore, bp.ore_robot_cost);
-                new_state.machine.0 +=1;
-            }
-        }
-        return new_state;
-    }
-
-    fn can_build(&self, blue_print:&BluePrint, action:&Action) -> bool {
-        match action {
-            Action::BuildGeodeBot => {
-               tuple_ge(self.ore, blue_print.geode_robot_cost)
-            }
-            Action::BuildObsidianBot => {
-                tuple_ge(self.ore, blue_print.obsidian_robot_cost)
-            }
-            Action::BuildClayBot => {
-                tuple_ge(self.ore, blue_print.clay_robot_cost)
-            }
-            Action::BuildOreBot => {
-                tuple_ge(self.ore, blue_print.ore_robot_cost)
-            }
-        }
-    }
-
+fn vector_add( a:[u16;4] , b:[u16;4]) -> [u16;4] {
+    let mut result: [u16;4] = [0,0,0,0];
+    result[0] = a[0] + b[0];
+    result[1] = a[1] + b[1];
+    result[2] = a[2] + b[2];
+    result[3] = a[3] + b[3];
+    return result;
 }
-
-fn tuple_add( a:(u32,u32,u32,u32) , b:(u32,u32,u32,u32))->(u32,u32,u32,u32) {
-    return (a.0+b.0, a.1+b.1, a.2+b.2, a.3+b.3);
-}
-
-fn tuple_sub( a:(u32,u32,u32,u32) , b:(u32,u32,u32,u32))->(u32,u32,u32,u32) {
-    return (a.0-b.0, a.1-b.1, a.2-b.2, a.3-b.3);
+fn vector_sub(a:[u16;4] , b:[u16;4]) -> [u16;4] {
+    let mut result: [u16;4] = [0,0,0,0];
+    result[0] = a[0] - b[0];
+    result[1] = a[1] - b[1];
+    result[2] = a[2] - b[2];
+    result[3] = a[3] - b[3];
+    return result;
 }
 
 
-fn tuple_ge( a:(u32,u32,u32,u32) , b:(u32,u32,u32,u32))->bool {
-    let ge_t = (a.0>=b.0, a.1>=b.1, a.2>=b.2, a.3>=b.3);
-    return ge_t.0 && ge_t.1 && ge_t.2 && ge_t.3;
+fn vector_ge(a:[u16;4] , b:[u16;4]) -> bool {
+       a[0] >= b[0] &&
+        a[1] >= b[1] &&
+        a[2] >= b[2] &&
+        a[3] >= b[3]
 }
+//
+// let blueprint = [
+// ore_bot_costs,
+// clay_bot_costs,
+// obsidian_bot_costs,
+// geode_bot_costs,
+// ];
+fn search(bp: [[u16; 4]; 4], max_turns: u16) -> u16 {
+
+    // let mut max_robots = [u16::MAX; 4];
+    //
+    // max_robots[0] = bp[0][0].max(bp[1][0]).max(bp[2][0]);
+    // max_robots[1] = bp[0][1].max(bp[1][1]).max(bp[2][1]);
+    // max_robots[2] = bp[2][2].max(bp[1][2]).max(bp[2][2]);
+
+    let mut max_geodes =0;
+    let mut max_time:u16 = max_turns;
+
+    let mut queue:VecDeque<State> = VecDeque::new();
+
+    let init_state = Default::default();
+    queue.push_back(init_state);
+
+    let mats = vec!{Material::Ore, Material::Clay, Material::Obsidian, Material::Obsidian};
+
+
+  while let Some(State {
+                     turn, ore, machine
+                 }) = queue.pop_front() {
+      for i in 0..bp.len() {
+          // if machine[i] == max_robots[i] {
+          //     continue;
+          // }
+
+          let costs = &bp[i];
+          let wait_time = (0..3)
+              .map(|idx| {
+                  match costs[idx] {
+                      cost if cost <= ore[idx] => 0 ,
+                      _ if machine[idx] == 0 => max_time + 1 ,
+                      _ => (costs[idx] - ore[idx] + machine[idx] - 1 ) / machine[idx],
+                  }
+              }).max().unwrap();
+
+          // time to build bot and for it to produce for 1 turn
+          let new_elapsed:u16 = turn + wait_time + 1;
+          if new_elapsed >= max_time {
+              continue;
+          }
+
+          let mut new_inventory: [u16; 4] = [0; 4];
+          for idx in 0..machine.len() {
+              new_inventory[idx] = ore[idx] + machine[idx] * (wait_time + 1) - costs[idx];
+          }
+
+          let mut new_bots = machine;
+          new_bots[i] += 1;
+
+          queue.push_back(State {
+              turn: new_elapsed,
+              ore: new_inventory,
+              machine: new_bots,
+          })
+      }
+      let geodes = ore[3] + machine[3] * (max_time - turn);
+      max_geodes = geodes.max(max_geodes);
+  }
+   max_geodes
+   }
 
 
 
 
-fn search(bp:&BluePrint, end_turn: u32) -> u32 {
-
-    // store best geode score at each time
-    // let mut cache: HashMap<i32, usize> = HashMap::new();
-    // for i in 0..=minutes {
-    //     cache.insert(i, 0);
-    // }
-
-    let mut queue: VecDeque<State> = VecDeque::new();
-    let mut visited: HashSet<State> = HashSet::new();
-let mut max_geo_machines = 0;
-   let mut max_geodes = 0;
-
-    let mut actions:Vec<Action>= Vec::new();
-    actions.push(Action::BuildGeodeBot);
-    actions.push(Action::BuildObsidianBot);
-    actions.push(Action::BuildClayBot);
-    actions.push(Action::BuildOreBot);
-
-    let mut start_state = Default::default();
-    queue.push_back(start_state);
-    let mut best_at_end:u32 = u32::MIN;
-
-        while !queue.is_empty()
-        {
-        let current = queue.pop_front().unwrap();
-        let init_state = current.clone();
-            max_geo_machines = cmp::max(init_state.machine.3, max_geo_machines);
-            max_geodes = cmp::max(init_state.ore.3, max_geodes);
 
 
-            visited.insert(current);
 
-        if current.turn > end_turn {
-    //        println!("- state at end of turn: \n \t{}", current);
-            best_at_end = cmp::max(best_at_end,current.ore.3);
-        } else {
-            for act in &actions {
-                if current.can_build(bp,act) {
-                    // if *act==Action::BuildGeodeBot {
-                    //     println!("can build geodebot from state: {current}");
-                    // }
-                     let c = current.tick_and_build(bp,act);
-
-                    // if *act==Action::BuildGeodeBot {
-                    //     println!("\t built geoodebot: {c}");
-                    // }
-                    // // println!("new state after action ({:?}) \t: {}",act, c);
-                    if !visited.contains(&c) {
-                        // println!("\t inserting {c}");
-                        queue.push_back(c);
-                    }
-                    break;
-                }
-            }
-            // if init_state.machine.3 > 0 {
-            //     print!("state has {} geode machines|-, " ,init_state);
-            // }
-
-            let c = current.tick();
-            // if init_state.machine.3 > 0 {
-            //     print!("new state has {} geodes ({}), ", c.ore.3, c);
-            // }
-
-            // println!("new state after no action  \t: {}", c);
-            if !visited.contains(&c) {
-                // println!("\t inserting {c}");
-                queue.push_back(c);
-            }
-        }
-    }
-
-    println!("max geo machines: {}", max_geo_machines);
-    println!("max geodes: {}", max_geodes);
-    return best_at_end;
-}
 
 
 fn part1() -> String {
@@ -320,17 +274,38 @@ fn part1() -> String {
     let mut v_blueprint:Vec<BluePrint>  = Vec::new();
     for line in lines {
         let bpi = line.parse::<BluePrintInput>().unwrap();
-        let bp = BluePrint::new(bpi);
+        // println!("bpi: {:?}", bpi);
+        let mut bp = BluePrint::new(bpi);
+        bp.id = bpi.id;
+        bp.ore_robot_cost = [bpi.ore_r_ore,0,0,0];
+        bp.clay_robot_cost =[bpi.clay_r_ore, 0,0,0];
+        bp.obsidian_robot_cost=[bpi.ob_r_ore, bpi.ob_r_clay,0,0];
+        bp.geode_robot_cost = [bpi.geo_r_ore,0,bpi.geo_r_ob,0];
+
         v_blueprint.push(bp);
+
+
     }
-    for r in v_blueprint {
-        println!("blueprint: {:?}", r);
-        let r_best = search(&r, PART1_TURNS);
-        println!("{}: {}",r.id, r_best );
+
+    let mut answer1 = 0;
+    let bp = v_blueprint[0];
+    // println!("bp: {:?}", bp);
+    for bp in v_blueprint {
+        let a_b = [
+            bp.ore_robot_cost,
+            bp.clay_robot_cost,
+            bp.obsidian_robot_cost,
+            bp.geode_robot_cost
+        ];
+        // println!("a_b: {:?}", a_b);
+        println!("bp: {:?}", bp);
+        let r = search(a_b, 24);
+        println!("\tblueprint id: {}, max_geodes: {}", bp.id, r);
+        answer1 += bp.id * (r as u32);
     }
-    let answer1 = String::new();
     return answer1.to_string();
 }
+
 
 fn part2() -> String {
     let p2_file = match TEST {
@@ -349,6 +324,38 @@ fn part2() -> String {
         }
     }
 
-    let mut answer2 =String::new();
-    return answer2;
+    let mut v_blueprint:Vec<BluePrint>  = Vec::new();
+    for line in lines {
+        let bpi = line.parse::<BluePrintInput>().unwrap();
+        // println!("bpi: {:?}", bpi);
+        let mut bp = BluePrint::new(bpi);
+        bp.id = bpi.id;
+        bp.ore_robot_cost = [bpi.ore_r_ore,0,0,0];
+        bp.clay_robot_cost =[bpi.clay_r_ore, 0,0,0];
+        bp.obsidian_robot_cost=[bpi.ob_r_ore, bpi.ob_r_clay,0,0];
+        bp.geode_robot_cost = [bpi.geo_r_ore,0,bpi.geo_r_ob,0];
+
+        v_blueprint.push(bp);
+
+
+    }
+
+    let mut answer2 = 1;
+    let bp = v_blueprint[0];
+    // println!("bp: {:?}", bp);
+    for bp in v_blueprint {
+        let a_b = [
+            bp.ore_robot_cost,
+            bp.clay_robot_cost,
+            bp.obsidian_robot_cost,
+            bp.geode_robot_cost
+        ];
+        // println!("a_b: {:?}", a_b);
+        println!("bp: {:?}", bp);
+        let r = search(a_b, 32);
+        println!("\tblueprint id: {}, max_geodes: {}", bp.id, r);
+        answer2 *= r as u32;
+    }
+
+    return answer2.to_string();
 }
