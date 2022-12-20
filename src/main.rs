82a77e88ed263a696ac4bf8960e7ba3e4d420a45
dnabre <alexander@happybunnylabs.com>
2022-12-20 11:11:03 -0700

    Day 20, part 1 and part 2 working, minimal clean good enough for me

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-#![allow(unused_mut)]
-
-
-use std::collections::{HashMap, LinkedList, VecDeque};
-use std::collections::HashSet;
-use std::{cmp, fmt};
-use std::fmt::{Display, Formatter};
 use std::fs;
-use std::time::{Instant,Duration};
-
-
-use parse_display::FromStr;
-
-
+use std::time::Instant;
 
 /*
     Advent of Code 2022: Day 20
@@ -31,10 +16,10 @@ const PART1_INPUT_FILENAME: &str = "data/day20/part1_input.txt";
 const PART2_TEST_FILENAME: &str = "data/day20/part2_test.txt";
 const PART2_INPUT_FILENAME: &str = "data/day20/part2_input.txt";
 
-const DECRYPTION_KEY:(i64,i64) = (1,811589153);
-const ITERATIONS:(usize,usize) = (1,10);
+const DECRYPTION_KEY: (i64, i64) = (1, 811589153);
+const ITERATIONS: (usize, usize) = (1, 10);
 
-const TEST: bool = true;
+const TEST: bool = false;
 
 fn main() {
     print!("Advent of Code 2022, Day ");
@@ -75,75 +60,10 @@ const LINE_ENDING: &'static str = "\r\n";
 const LINE_ENDING: &'static str = "\n";
 
 
-struct CList {
-    vec:Vec<i64>,
-    size:usize,
-    wrap:i64,
-}
-
-impl Display for CList {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let mut sb = String::new();
-        sb.push('[');
-        if self.size > 1 {
-            for i in 0..(self.wrap as usize) {
-                sb.push_str(self.vec[i].to_string().as_str());
-                sb.push(',');
-                sb.push(' ');
-            }
-            sb.push_str(self.vec[self.vec.len()-1 as usize].to_string().as_str());
-        } else {
-            if self.size == 1 {
-                sb.push_str(self.vec[0].to_string().as_str());
-            }
-        }
-        sb.push(']');
-        return write!(f, "{}", sb);
-    }
-}
-impl CList {
-    fn new(v:&Vec<i64> ) -> Self {
-        CList {
-            vec: v.clone(),
-            size: v.len(),
-            wrap: (v.len() - 1) as i64,
-        }
-    }
-
-    fn pop_front(&mut self) -> i64 {
-        let r = self.vec.remove(0);
-        return r;
-    }
-
-    fn size(&self) -> usize {
-        return self.vec.len();
-    }
-    fn insert(&mut self, index:i64, value:i64) {
-        let mut new_index = index;
-        if index.abs() > self.wrap  {
-            if index < 0 {
-                let pos_index = index.abs();
-                let w_index = index % self.wrap;
-                let new_neg_index = -1 * w_index;
-                let new_index = self.wrap + new_neg_index;
-                println!("negative index: {index} , converted to {new_index}");
-            } else {
-                new_index = index % self.wrap;
-            }
-        }
-        assert!(new_index >= 0);
-        assert!(new_index < self.size as i64);
-
-        self.vec.insert(new_index as usize,value);
-
-
-    }
-}
-
 #[derive(Clone)]
 struct Number {
     original_index: usize,
-    move_by: i64
+    move_by: i64,
 }
 
 impl PartialEq for Number {
@@ -152,16 +72,17 @@ impl PartialEq for Number {
     }
 }
 
-fn shift_element<Number>(vec    : &Vec<Number>,
-                    index  : usize,
-                    offset : i64) -> Vec<Number>
-where T:Clone+PartialEq
+//generic is used so that the iter mess works properly
+fn shift_element<T>(vec: &Vec<T>,
+                    index: usize,
+                    offset: i64) -> Vec<T>
+    where T: Clone + PartialEq
 {
     let len = vec.len() as i64;
 
     // concat three copies of the list
-    let mut tripled:Vec<T> = Vec::with_capacity(vec.len() * 3);
-    for _  in 0..3 {
+    let mut tripled: Vec<T> = Vec::with_capacity(vec.len() * 3);
+    for _ in 0..3 {
         for v in vec {
             tripled.push(v.clone());
         }
@@ -170,20 +91,19 @@ where T:Clone+PartialEq
 
     //wrap offset into tripled lists
     // just mods by len-1, but does witchcraft to handle mod of negative value
-    let offset = (offset % (len-1) + len) % len
-                        + if offset > 0 {1}
-                        else {0};
+    let offset = (offset % (len - 1) + len) % len
+        + if offset > 0 { 1 } else { 0 };
 
     //build return vector
     [
-    // make vec of target value
+        // make vec of target value
         vec![vec[index].clone()],
         tripled.into_iter()
-            .skip( index +offset as usize)
+            .skip(index + offset as usize)
             .filter(|el| *el != vec[index])
-            .take(vec.len() -1)
+            .take(vec.len() - 1)
             .collect()
-        ].concat()
+    ].concat()
 }
 
 
@@ -195,7 +115,7 @@ fn part1() -> String {
     let data1_s =
         fs::read_to_string(p1_file).expect(&*format!("error opening file {}", p1_file));
 
-       let mut lines: Vec<&str> = data1_s.trim().split(LINE_ENDING).collect();
+    let lines: Vec<&str> = data1_s.trim().split(LINE_ENDING).collect();
     let l_num = lines.len();
     if TEST {
         println!("\t read {} lines from {}", l_num, p1_file);
@@ -204,26 +124,26 @@ fn part1() -> String {
         }
     }
 
-    let decryption_key:i64 = DECRYPTION_KEY.0;
-    let iterations:usize = ITERATIONS.0;
-    let  orig_numbers:Vec<i64> = lines.into_iter().map(|n| n.parse::<i64>().unwrap()).collect();
+    let decryption_key: i64 = DECRYPTION_KEY.0;
+    let iterations: usize = ITERATIONS.0;
+    let orig_numbers: Vec<i64> = lines.into_iter().map(|n| n.parse::<i64>().unwrap()).collect();
 
-    let mut seq :Vec<Number> = orig_numbers.iter()
+    let mut seq: Vec<Number> = orig_numbers.iter()
         .enumerate()
         .map(|(original_index, &move_by)|
             Number {
                 original_index,
-                move_by: move_by * decryption_key
+                move_by: move_by * decryption_key,
             })
         .collect();
-  //  println!("seq: {:?}", seq);
+    //  println!("seq: {:?}", seq);
 
     let len = seq.len();
 
     for _ in 0..iterations {
         for i in 0..len {
             let index = seq.iter().position(
-                |num| num.original_index == i ).unwrap();
+                |num| num.original_index == i).unwrap();
 
             let offset = seq[index].move_by;
             let shifted = shift_element(&seq, index, offset);
@@ -234,7 +154,7 @@ fn part1() -> String {
     let index_zero = seq.iter()
         .position(|num| num.move_by == 0).unwrap();
 
-   let t =  seq[(index_zero + 1000) % len].move_by
+    let t = seq[(index_zero + 1000) % len].move_by
         + seq[(index_zero + 2000) % len].move_by
         + seq[(index_zero + 3000) % len].move_by;
 
@@ -251,7 +171,7 @@ fn part2() -> String {
     let data2_s =
         fs::read_to_string(p2_file).expect(&*format!("error opening file {}", p2_file));
 
-    let mut lines: Vec<&str> = data2_s.trim().split(LINE_ENDING).collect();
+    let lines: Vec<&str> = data2_s.trim().split(LINE_ENDING).collect();
     let l_num = lines.len();
     if TEST {
         println!("\t read {} lines from {}", l_num, p2_file);
@@ -261,16 +181,16 @@ fn part2() -> String {
     }
 
 
-    let decryption_key:i64 = DECRYPTION_KEY.1;
-    let iterations:usize = ITERATIONS.1;
-    let  orig_numbers:Vec<i64> = lines.into_iter().map(|n| n.parse::<i64>().unwrap()).collect();
+    let decryption_key: i64 = DECRYPTION_KEY.1;
+    let iterations: usize = ITERATIONS.1;
+    let orig_numbers: Vec<i64> = lines.into_iter().map(|n| n.parse::<i64>().unwrap()).collect();
 
-    let mut seq :Vec<Number> = orig_numbers.iter()
+    let mut seq: Vec<Number> = orig_numbers.iter()
         .enumerate()
         .map(|(original_index, &move_by)|
             Number {
                 original_index,
-                move_by: move_by * decryption_key
+                move_by: move_by * decryption_key,
             })
         .collect();
     //  println!("seq: {:?}", seq);
@@ -280,7 +200,7 @@ fn part2() -> String {
     for _ in 0..iterations {
         for i in 0..len {
             let index = seq.iter().position(
-                |num| num.original_index == i ).unwrap();
+                |num| num.original_index == i).unwrap();
 
             let offset = seq[index].move_by;
             let shifted = shift_element(&seq, index, offset);
@@ -291,11 +211,11 @@ fn part2() -> String {
     let index_zero = seq.iter()
         .position(|num| num.move_by == 0).unwrap();
 
-    let t =  seq[(index_zero + 1000) % len].move_by
+    let t = seq[(index_zero + 1000) % len].move_by
         + seq[(index_zero + 2000) % len].move_by
         + seq[(index_zero + 3000) % len].move_by;
 
 
-    let mut answer2  = t.to_string();
+    let answer2 = t.to_string();
     return answer2;
 }
