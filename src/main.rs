5c61a553b9ac97bb99a23601cf949286b01af4cd
matthew j. barrick <alexander@happybunnylabs.com>
2022-12-21 17:20:47 -0700

    Day 21, switched part 2 from i64 to Decimal, so that integer division doesn't screw things up

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-#![allow(unused_mut)]
-
-
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fmt;
@@ -15,7 +9,6 @@ use std::thread::current;
 use std::time::Instant;
 use rust_decimal::prelude::*;
 use rust_decimal_macros::dec;
-
 use parse_display::FromStr;
 
 
@@ -39,7 +32,7 @@ const PART2_TEST_FILENAME: &str = "data/day21/part2_test.txt";
 const PART2_INPUT_FILENAME: &str = "data/day21/part2_input.txt";
 
 
-const TEST: bool = false;
+const TEST: bool = true;
 
 fn main() {
     print!("Advent of Code 2022, Day ");
@@ -123,6 +116,22 @@ impl fmt::Display for MonkeyOp {
 }
 
 
+
+#[derive(Hash, PartialEq,Debug,Clone)]
+enum MonkeyOpD {
+    Number (Decimal),
+    Eq {left:String,op:Op,right:String},
+}
+
+
+
+
+
+
+
+
+
+
 fn parse_monkeys(lines: &mut Vec<&str>) -> HashMap<String, MonkeyOp> {
     let mut name_to_op: HashMap<String, MonkeyOp> = HashMap::new();
     let mut v_namelist: Vec<String> = Vec::new();
@@ -140,6 +149,47 @@ fn parse_monkeys(lines: &mut Vec<&str>) -> HashMap<String, MonkeyOp> {
     }
     name_to_op
 }
+
+fn parse_monkeys_d(lines: &mut Vec<&str>) -> HashMap<String, MonkeyOpD> {
+    let mut name_to_op: HashMap<String, MonkeyOpD> = HashMap::new();
+    let mut v_namelist: Vec<String> = Vec::new();
+
+
+    for ln in lines {
+        let (l, mut r) = ln.split_once(":").unwrap();
+
+        let m_name: String = String::from(l);
+        if m_name.ne("root") {
+            v_namelist.push(m_name.clone());
+        }
+        let m_op = r.trim().parse::<MonkeyOp>().unwrap();
+
+        let m_op_d = monkey_op_d(m_op);
+
+        name_to_op.insert(m_name, m_op_d);
+    }
+    name_to_op
+}
+
+fn monkey_op_d(p0: MonkeyOp) -> MonkeyOpD {
+  match p0 {
+      MonkeyOp::Number(x) => {
+          MonkeyOpD::Number(Decimal::from_i64(x).unwrap())
+      }
+      MonkeyOp::Eq { left, op, right } => {
+          let l = left;
+          let r = right;
+          let o = op;
+          MonkeyOpD::Eq {
+              left:l,
+              op:o,
+              right:r,
+          }
+      }
+  }
+}
+
+
 
 
 fn do_monkey_op(left_v: i64, right_v: i64, op: Op) -> i64 {
@@ -161,9 +211,8 @@ fn do_monkey_op_d(left_v: Decimal, right_v: Decimal, op: Op) -> Decimal {
 }
 
 
-
-fn solve_map_for<'a>(solve_for: &'a str, name_to_op:&'a HashMap<String, MonkeyOp>, mut value_hash: &'a mut HashMap<&'a str,i64> ) -> i64{
-       let mut todo = Vec::new();
+fn solve_map_for0<'a>(solve_for: &'a str, name_to_op:&'a HashMap<String, MonkeyOp>, mut value_hash: &'a mut HashMap<&'a str,i64> ) -> i64{
+    let mut todo = Vec::new();
     todo.push(solve_for);
     while !todo.is_empty() {
         let mut current = todo.pop().unwrap();
@@ -181,6 +230,48 @@ fn solve_map_for<'a>(solve_for: &'a str, name_to_op:&'a HashMap<String, MonkeyOp
                     let (left_v, right_v) = (value_hash.get(left.as_str()).unwrap(),
                                              value_hash.get(right.as_str()).unwrap());
                     let result = do_monkey_op(*left_v, *right_v, *op);
+                    value_hash.insert(current, result);
+                } else {
+                    // we're missing a value, so retry current op
+                    todo.push(current);
+                    if !value_hash.contains_key(left.as_str()) {
+                        // we need left
+                        todo.push(left);
+                    }
+                    if !value_hash.contains_key(right.as_str()) {
+                        todo.push(right);
+                    }
+                }
+            }
+        }
+    }
+
+    let solution = *value_hash.get(solve_for).unwrap();
+
+    return solution;
+}
+
+
+
+fn solve_map_for<'a>(solve_for: &'a str, name_to_op:&'a HashMap<String, MonkeyOpD>, mut value_hash: &'a mut HashMap<&'a str,Decimal> ) -> Decimal{
+       let mut todo = Vec::new();
+    todo.push(solve_for);
+    while !todo.is_empty() {
+        let mut current = todo.pop().unwrap();
+        if value_hash.contains_key(current) {
+            continue;
+        }
+        let monkey_op = name_to_op.get(current).unwrap();
+        match monkey_op {
+            MonkeyOpD::Number(n) => {
+                value_hash.insert(current, (*n).into());
+                continue;
+            }
+            MonkeyOpD::Eq { left, op, right } => {
+                if value_hash.contains_key(left.as_str()) && value_hash.contains_key(right.as_str()) {
+                    let (left_v, right_v) = (value_hash.get(left.as_str()).unwrap(),
+                                             value_hash.get(right.as_str()).unwrap());
+                    let result = do_monkey_op_d(*left_v, *right_v, *op);
                     value_hash.insert(current, result);
                 } else {
                     // we're missing a value, so retry current op
@@ -225,7 +316,7 @@ fn part1() -> String {
 
     let mut name_to_op = parse_monkeys(&mut lines);
     let mut value_hash: HashMap<&str, i64> = HashMap::new();
-    let final_value = solve_map_for("root", & name_to_op, &mut value_hash);
+    let final_value = solve_map_for0("root", & name_to_op, &mut value_hash);
 
     let mut answer1 = final_value.to_string();
     return answer1;
@@ -248,19 +339,19 @@ fn part2() -> String {
         }
     }
 
-    let mut name_to_op = parse_monkeys(&mut lines);
+    let mut name_to_op:HashMap<String,MonkeyOpD> = parse_monkeys_d(&mut lines);
 
 
 
     let nothing = String::from("nothing");
 
-    let root_op = name_to_op.get("root").unwrap();
+    let root_op:&MonkeyOpD = name_to_op.get("root").unwrap();
 
 
 
     let (root_left, root_right) = match root_op {
-        MonkeyOp::Number(_) => {(&nothing, &nothing)}
-        MonkeyOp::Eq { left, op, right } => { (left,right) }
+        MonkeyOpD::Number(_) => {(&nothing, &nothing)}
+        MonkeyOpD::Eq { left, op, right } => { (left,right) }
     };
 
 
@@ -268,7 +359,7 @@ fn part2() -> String {
     let mut solve_test;
     let l_deps = test_for_depends(root_left, PART2_MNAME, &name_to_op);
     let r_deps = test_for_depends(root_right, PART2_MNAME, &name_to_op);
-    let mut value_map:HashMap<&str,i64> = HashMap::new();
+    let mut value_map:HashMap<&str,Decimal> = HashMap::new();
     if l_deps {
         target = solve_map_for(root_right, &name_to_op, &mut value_map);
         solve_test = root_left;
@@ -283,22 +374,22 @@ fn part2() -> String {
 
 
     let left = target;
-    let mut inital:HashMap<&str,i64> = HashMap::new();
+    let mut inital:HashMap<&str,Decimal> = HashMap::new();
     let mut right = solve_map_for(solve_test, &name_to_op, &mut inital);
     let base:i64 = 2;
 
-    let (mut min,mut max) = (-1 * base.pow(44),base.pow(44));
-    let mut answer=0;
+    let (mut min,mut  max):(Decimal,Decimal) = (Decimal::from_i64(-1 * base.pow(44)).unwrap(),Decimal::from_i64(base.pow(44)).unwrap());
+    let mut answer=dec!(0);
 
 
 
-    let test1 = 1;
-    let mut new_value_map:HashMap<&str,i64> = HashMap::new();
+    let test1 = dec!(1);
+    let mut new_value_map:HashMap<&str,Decimal> = HashMap::new();
     new_value_map.insert(PART2_MNAME, test1);
     let r1 = solve_map_for(solve_test, &name_to_op, &mut new_value_map);
 
-    let test2 = 100;
-    let mut new_value_map:HashMap<&str,i64> = HashMap::new();
+    let test2 = dec!(100);
+    let mut new_value_map:HashMap<&str,Decimal> = HashMap::new();
     new_value_map.insert(PART2_MNAME, test2);
     let r2 = solve_map_for(solve_test, &name_to_op, &mut new_value_map);
 
@@ -307,24 +398,24 @@ fn part2() -> String {
 
 
     while left != right {
-
+        let d_two = Decimal::from_i64(2).unwrap();
         if right > target {
             if positive {
-                max = min + (max - min) / 2;
+                max = min + (max - min) / d_two;
             } else {
-                min = min + (max - min) / 2;
+                min = min + (max - min) /  d_two;
             }
         } else {
             if positive {
-                min = min + (max - min) / 2;
+                min = min + (max - min) /  d_two;
             } else {
-                max = min + (max - min) / 2;
+                max = min + (max - min) /  d_two;
             }
         }
 
-        let mut test = min + (max - min) /2;
+        let mut test = min + (max - min) / d_two;
 
-        let mut new_value_map:HashMap<&str,i64> = HashMap::new();
+        let mut new_value_map:HashMap<&str,Decimal> = HashMap::new();
         new_value_map.insert(PART2_MNAME, test);
         let result = solve_map_for(solve_test, &name_to_op, &mut new_value_map);
 
@@ -382,7 +473,7 @@ fn search_for_depend<'a>(root: &'a str, goal: &'a str, op_map: &'a HashMap<Strin
       }
     return stack.clone();
 }
-fn test_for_depends<'a>(root: &'a str, goal: &'a str, op_map: &'a HashMap<String, MonkeyOp>) ->  bool {
+fn test_for_depends<'a>(root: &'a str, goal: &'a str, op_map: &'a HashMap<String, MonkeyOpD>) ->  bool {
     let mut stack:Vec<&str> = Vec::new();
 
     if root.ne(goal) {
@@ -392,11 +483,11 @@ fn test_for_depends<'a>(root: &'a str, goal: &'a str, op_map: &'a HashMap<String
         let current = stack.pop().unwrap();
         let t_op = op_map.get(current).unwrap();
         match t_op {
-            MonkeyOp::Number(_) => {
+            MonkeyOpD::Number(_) => {
                 continue;
 
             }
-            MonkeyOp::Eq { left, op, right } => {
+            MonkeyOpD::Eq { left, op, right } => {
                 if goal.eq(left){
                     stack.push(current);
                     break 'todo;
