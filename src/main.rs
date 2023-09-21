13edc7fc575e378a0662e0bc0e9bdfacbc305439
dnabre <dnabre@happybunnylabs.com>
2023-09-21 13:08:34 -0600

    temp change to util for soemthing random

--- src/main.rs
+++ src/main.rs
@@ -35,27 +35,27 @@ const PART2_INPUT_FILENAME: &str = "data/day14/part2_input.txt";
 const TEST: bool = true;
 
 fn main() {
-    print!("Advent of Code 2022, Day ");
-    println!("14");                           // insert Day
+    // print!("Advent of Code 2022, Day ");
+    // println!("14");                           // insert Day
 
 
     let start1 = Instant::now();
     let answer1 = part1();
     let duration1 = start1.elapsed();
 
-    println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
-    //
+    // println!("\t Part 1: {answer1} ,\t time: {:?}", duration1);
+    // //
     // if TEST {
     //     assert_eq!(answer1, TEST_ANSWER.0.to_string());
     // } else {
     //     assert_eq!(answer1, INPUT_ANSWER.0.to_string());
     // }
 
-    let start2 = Instant::now();
-    let answer2 = part2();
-    let duration2 = start2.elapsed();
-
-    println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
+    // let start2 = Instant::now();
+    // let answer2 = part2();
+    // let duration2 = start2.elapsed();
+    //
+    // println!("\t Part 2: {answer2} ,\t time: {:?}", duration2);
 
     // if TEST {
     //     assert_eq!(answer2, TEST_ANSWER.1.to_string());
@@ -63,7 +63,7 @@ fn main() {
     //     assert_eq!(answer2, INPUT_ANSWER.1.to_string());
     // }
 
-    println!("----------\ndone");
+    // println!("----------\ndone");
 }
 
 #[cfg(windows)]
@@ -86,17 +86,49 @@ fn part1() -> String {
         fs::read_to_string(p1_file).expect(&*format!("error opening file {}", p1_file));
     let mut lines: Vec<&str> = data1_s.trim().split("\n").collect();
     let l_num = lines.len();
-    if TEST {
-        println!("\t read {} lines from {}", l_num, p1_file);
-        if l_num == 1 {
-            println!("\t\t line read has length: {}", lines[0].len());
+    // if TEST {
+    //     println!("\t read {} lines from {}", l_num, p1_file);
+    //     if l_num == 1 {
+    //         println!("\t\t line read has length: {}", lines[0].len());
+    //     }
+    // }
+    // let data1_ss = data1_s.trim();
+    // let split_lines:Vec<&str> = data1_ss.split(LINE_ENDING).collect();
+    // for l in split_lines {
+    //     println!("{l}");
+    // }
+
+    let p1_file = "in.info";
+    let data1_s =
+        fs::read_to_string(p1_file).expect(&*format!("error opening file {}", p1_file));
+    let mut lines: Vec<&str> = data1_s.trim().split("\n").collect();
+    let l_num = lines.len();
+    //println!("lines = {}", l_num);
+
+
+    for whole in lines {
+        if whole.starts_with("BookmarkPageNumber")  ||
+            whole.starts_with("NumberOfPages") ||
+            whole.starts_with("PageMediaNumber")
+            {
+            let ps = whole.split_once(":").unwrap();
+            let (l,r) = ps;
+            let n:i32 = r.trim().parse().unwrap();
+            let n2 = n-1;
+
+
+        //    println!("{whole}");
+            println!("{l}: {n2}");
+
+
+         //   println!("{:?} n={n}",ps );
+        }
+        else {
+            println!("{whole}");
         }
     }
-    let data1_ss = data1_s.trim();
-    let split_lines:Vec<&str> = data1_ss.split(LINE_ENDING).collect();
-    for l in split_lines {
-        println!("{l}");
-    }
+
+
 
     let answer1 = String::new();
     return answer1.to_string();
