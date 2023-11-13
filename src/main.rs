314dc75bb6e2e56a2471fb90089034cd76978e26
dnabre <dnabre@happybunnylabs.com>
2023-11-13 08:48:40 -0700

    Working on Day 24 calulating position at time t properly

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@
 #![allow(unused_assignments)]
 
 
+use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Formatter};
 use std::fs;
 use std::time::Instant;
 
-use ndarray::{Array, Array2, ArrayBase, OwnedRepr, Dim};
 
 /*
     Advent of Code 2022: Day 24`
@@ -24,6 +24,7 @@ const TEST_ANSWER: (i64, i64) = (20, 110);
 const INPUT_ANSWER: (i64, i64) = (4195, 1069);
 
 const PART1_TEST_FILENAME: &str = "data/day24/part1_test.txt";
+const PART1B_TEST_FILENAME: &str = "data/day24/part1b_test.txt";
 const PART1_INPUT_FILENAME: &str = "data/day24/part1_input.txt";
 
 const PART2_TEST_FILENAME: &str = "data/day24/part2_test.txt";
@@ -103,10 +104,84 @@ impl std::fmt::Display for Direction {
     }
 }
 
-struct Blizzard {
-    row:usize,
-    col:usize,
-    dir:Direction
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Coord {
+    row: usize,
+    col: usize,
+}
+
+impl Display for Coord {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f,"@[row={}, col={}]", self.row, self.col )
+    }
+}
+
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Blizz {
+    row: usize,
+    col: usize,
+    dir: Direction
+}
+
+impl Blizz {
+     fn pos_at_time(&self, t: i32, (max_row, max_col):(usize,usize) ) -> Coord {
+         let mut r:i32 = -1;
+         let mut c:i32 = -1;
+
+         let (i_max_row, i_max_col) = (max_row as i32, max_col as i32);
+         let (i_row, i_col) = (self.row as i32, self.col as i32);
+
+
+         match self.dir{
+             Direction::Up => {
+                 c = i_col;
+                 // messy to ignore modulo of negative
+                 r =  (i_row - 1 +  (t * (i_max_row -2) -t))
+                     % (i_max_row - 2) + 1 ;
+
+             }
+             Direction::Down => {
+                 c = i_col;
+                 r =  ((i_row - 1 + t) % (i_max_row - 2)) + 1 ;
+             }
+             Direction::Left => {
+                 r = i_row;
+                 // messy to ignore modulo of negative
+                 c =   (i_col - 1 +  (t * (i_max_col -2) -t))
+                     % (i_max_col - 2) + 1 ;
+
+
+             }
+             Direction::Right => {
+                 r = i_row;
+                 c= ((i_col - 1 + t) % (i_max_col - 2)) + 1 ;
+             }
+         }
+
+        return Coord{row: r as usize, col: c as usize};
+    }
+}
+
+impl Display for Blizz {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+       write!(f, "@[row={}, col={}] dir={}", self.row, self.col, self.dir)
+    }
+}
+
+impl Blizz {
+    fn new(r:usize, c:usize, d:char) -> Blizz {
+        let dir = match d
+        {
+            '^' => {Direction::Up}
+            'v' => {Direction::Down}
+            '<' => {Direction::Left}
+            '>' => {Direction::Right}
+            _ => { panic!("bad direction given in Blizz::new -> {d}")}
+        };
+        return Blizz{ row: r, col: c, dir, };
+    }
 }
 
 
@@ -127,41 +202,91 @@ fn part1() -> String {
     }
     let data1_ss = data1_s.trim();
     let split_lines: Vec<&str> = data1_ss.split(LINE_ENDING).collect();
-    let dims@(rows,cols) = (split_lines.len(), split_lines[0].len());
-    println!("dims: {:?}", dims);
 
+    let num_rows = split_lines.len();
+    let num_cols = split_lines[0].len();
 
-   let mut char_buffer = String::new();
-    for l in split_lines {
-        char_buffer.push_str(l);
-    }
-    let  v:Vec<char> = char_buffer.chars().collect();
-    let v2:Vec<char>  = v[rows..(v.len() - cols)].to_vec();
+    let dims = (num_rows, num_cols);
+    println!("rows: {}, cols: {}", num_rows, num_cols);
 
-    let mut v3 = v2.clone();
-    v3.retain_mut(|c| *c != '#');
+    let start_coord = Coord{ row: 0, col: 1 };
+    let end_coord = Coord{row: num_rows -1, col: num_cols-2};
 
+    let dims = ( num_rows,  num_cols);
 
+    println!("grid dimensions: {:?}", dims);
+    let v1:Vec<Vec<char>> = split_lines.iter().map(|f| f.chars().collect()).collect();
 
-   let init_grid =
-       Array::from_shape_vec((rows-2,cols-2), v3.to_vec()).unwrap();
-    println!("{:?}\n", init_grid);
+    let mut wall_set:HashSet<Coord> = HashSet::new();
+    let mut blizz_vec:Vec<Blizz> = Vec::new();
+    let mut coord_to_blizz:HashMap<Coord, Blizz> = HashMap::new();
 
-    println!("init_grid.dim().0 = {}", init_grid.dim().0); // # rows
-    println!("init_grid.dim().1 = {}", init_grid.dim().1); // # cols
-println!();
-    //let mut blizzards:Vec<Blizzard> = Vec::new();
-    for row in 0..init_grid.dim().0 {
-        for col in 0..init_grid.dim().1 {
-            let c = init_grid[[row,col]];
-            print!("{}", c);
+    for r in 0..num_rows {
+        for c in 0..num_cols {
+            match v1[r][c] {
+                '#' => {wall_set.insert(Coord{row: r, col: c});}
+                '.' => {/*ignore empty spots*/ }
+                d_char => {
+                    let b = Blizz::new(r,c,d_char);
+                    coord_to_blizz.insert(Coord{row: r, col: c}, b);
+                    blizz_vec.push(b);
+                }
+            }
         }
-        println!();
+    }
+
+    println!("found {} blizzards", blizz_vec.len());
+    //
+    // for b in blizz_vec.iter() {
+    //     println!("start {}", b);
+    //     for t in 0..10 {
+    //         let c = b.pos_at_time(t,dims);
+    //         println!("\t at time {t}, {c}");
+    //     }
+    //     println!();
+    // }
+
+
+    for t in 0..=5 {
+        println!(" === time {t} ===");
+        for r in 0..num_rows {
+            for c in 0..num_cols {
+                let mut empty = true;
+                let co = Coord { row: r, col: c };
+                if co == start_coord {
+                    print!("B");
+                    empty = false;
+                    continue
+                } else if co == end_coord {
+                    print!("E");
+                    empty = false;
+                    continue
+                } else if wall_set.contains(&co) {
+                    print!("#");
+                    empty = false;
+                    continue
+                }
+
+                for b in &blizz_vec {
+                    let t_coord = b.pos_at_time(t, dims);
+                    if t_coord == co {
+                        print!("{}", b.dir);
+                        empty = false;
+                        break;
+                    }
+
+                }
+                if empty {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+        println!("===================\n");
     }
 
 
-
-    let answer1 = String::new();
+            let answer1 = String::new();
     return answer1.to_string();
 }
 
