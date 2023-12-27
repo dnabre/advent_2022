1bc950669624bde6e20b38b4ce4b8f77d289a5a0
dnabre <dnabre@happybunnylabs.com>
2023-12-27 11:38:08 -0700

    Day 22 template (2023-version)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::collections::{BTreeSet, HashSet};
-use std::hash::Hash;
-use std::ops::Range;
-use std::time::{Instant, SystemTime};
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(dead_code)]
+#![allow(unused_assignments)]
+
+
+use std::time::Instant;
 
 /*
-    Advent of Code 2023: Day 25
+    Advent of Code 2022: Day 22
         part1 answer:   514786
         part2 answer:
 
@@ -12,15 +16,17 @@ use std::time::{Instant, SystemTime};
 const ANSWER: (&str, &str) = ("514786", "Button Pressed");
 
 fn main() {
-    let _filename_test = "data/day25/test_input_01.txt";
-    let _filename_test2 = "data/day25/test_input_02.txt";
+    let _filename_test = "data/day22/test_input_01.txt";
+    let _filename_test2 = "data/day22/test_input_02.txt";
 
-    let filename_part1 = "data/day25/part1_input.txt";
+    let filename_part1 = "data/day22/part1_input.txt";
+    let filename_part2 = "data/day22/part2_input.txt";
 
-    println!("Advent of Code, Day 25");
+
+    println!("Advent of Code, Day 22");
     println!("    ---------------------------------------------");
     let start1 = Instant::now();
-    let answer1 = part1(filename_part1);
+    let answer1 = part1(_filename_test);
     let duration1 = start1.elapsed();
 
     println!("\t Part 1: {:14} time: {:?}", answer1, duration1);
@@ -32,7 +38,7 @@ fn main() {
     }
 
     let start2 = Instant::now();
-    let answer2 = part2();
+    let answer2 = part2(_filename_test2);
     let duration2 = start2.elapsed();
 
     println!("\t Part 2: {:14} time: {:?}", answer2, duration2);
@@ -45,97 +51,27 @@ fn main() {
     println!("    ---------------------------------------------");
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-struct Node {
-    id: usize,
-    adjacent: BTreeSet<usize>,
-}
-
-type Graph = (V, E);
-type V = HashSet<String>;
-type E = Vec<(String, String)>;
-
-fn get_random(seed: &mut u128, range: Range<usize>) -> usize {
-    const M: u128 = 9223372036854775808u128;
-    const A: u128 = 1103515245u128;
-    const C: u128 = 12345u128;
-
-    *seed = (A * *seed + C) % M;
-    let b = range.end - range.start;
-    let offset = (*seed % b as u128) as usize;
-    return range.start + offset;
-}
 
 fn part1(input_file: &str) -> String {
-    let lines = advent_2023::file_to_lines(input_file);
+    let lines = advent_2022::file_to_lines(input_file);
+    println!("\n part1 reading {} lines from {}", lines.len(), input_file);
+    for i in 0..lines.len() {
 
-    let (vertices, edges) = parse_input(&lines);
-
-    let mut prng_seed = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_nanos();
-
-    loop {
-        let mut vertices = vertices.clone();
-        let mut edges = edges.clone();
-        while vertices.len() > 2 {
-            let i = get_random(&mut prng_seed, 0..edges.len());
-            let (v1, v2) = edges[i].clone();
-
-            edges.swap_remove(i);
-            vertices.remove(&v1);
-            vertices.remove(&v2);
-
-            let new_v = format!("{}:{}", v1, v2);
-            vertices.insert(new_v.clone());
-            for (e1, e2) in edges.iter_mut() {
-                if *e1 == v1 || *e1 == v2 {
-                    *e1 = new_v.clone()
-                }
-                if *e2 == v1 || *e2 == v2 {
-                    *e2 = new_v.clone()
-                }
-            }
-
-            let mut j = 0;
-            while j < edges.len() {
-                let (e1, e2) = &edges[j];
-                if e1 == e2 {
-                    edges.swap_remove(j);
-                } else {
-                    j += 1;
-                }
-            }
-        }
-        if edges.len() == 3 {
-            return vertices
-                .iter()
-                .map(|s| s.split(':').count())
-                .product::<usize>()
-                .to_string();
-        }
+        println!("{i:3}\t len: {:5}", lines[i].len())
     }
+
+    let answer = 0;
+    return answer.to_string();
 }
 
-fn parse_input(lines: &Vec<String>) -> Graph {
-    let n = lines.len();
-    let mut vertices: HashSet<String> = HashSet::with_capacity(n);
-    let mut edges: Vec<(String, String)> = Vec::with_capacity(n * n);
+fn part2(input_file: &str) -> String {
+    let lines = advent_2022::file_to_lines(input_file);
+    println!("\n part2 reading {} lines from {}", lines.len(), input_file);
+    for i in 0..lines.len() {
 
-    for i in 0..n {
-        let line = &lines[i];
-        let (left, right_side) = line.split_once(":").unwrap();
-        vertices.insert(left.to_string());
-        for right in right_side.split_whitespace() {
-            vertices.insert(right.to_string());
-            edges.push((left.to_string(), right.to_string()));
-        }
+        println!("{i:3}\t len: {:5}", lines[i].len())
     }
-    return (vertices, edges);
-}
 
-fn part2() -> String {
-    let answer = "Button Pressed";
+    let answer = 0;
     return answer.to_string();
 }
